3f00fd10e87b5c3abbc0ded141eca945c28cc462
Londongrad <Teslagradus@gmail.com>
2025-11-11 23:46:46 +0900

    feat(population-domain): add HappinessLevel value object

---
+++ src/services/population/Matrix.Population.Domain/ValueObjects/HappinessLevel.cs
@@ -0,0 +1,14 @@
+﻿using Matrix.BuildingBlocks.Domain;
+
+namespace Matrix.Population.Domain.ValueObjects
+{
+    public readonly struct HappinessLevel(int value)
+    {
+        public int Value { get; } = GuardHelper.AgainstOutOfRange(value, 0, 100, nameof(Value));
+
+        public static HappinessLevel From(int value) => new(value);
+
+        public HappinessLevel WithDelta(int delta)
+            => From(Math.Clamp(Value + delta, 0, 100));
+    }
+}
