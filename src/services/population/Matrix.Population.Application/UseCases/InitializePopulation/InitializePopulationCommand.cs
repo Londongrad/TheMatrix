280dacc78b03c5f9b20c11e50cf6424f7263e1e8
Londongrad <Teslagradus@gmail.com>
2025-11-11 23:45:06 +0900

    feat(cqrs-commands): implement InitializePopulation command

---
+++ src/services/population/Matrix.Population.Application/UseCases/InitializePopulation/InitializePopulationCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Matrix.Population.Application.UseCases.InitializePopulation
+{
+    public sealed record InitializePopulationCommand(
+        int PeopleCount,
+        int? RandomSeed) : IRequest<Unit>;
+}
---
+++ src/services/population/Matrix.Population.Application/UseCases/InitializePopulation/InitializePopulationHandler.cs
@@ -0,0 +1,33 @@
+﻿using Matrix.Population.Application.Abstractions;
+using Matrix.Population.Domain.Services;
+using MediatR;
+
+namespace Matrix.Population.Application.UseCases.InitializePopulation
+{
+    public sealed class InitializePopulationHandler(
+        IPersonWriteRepository personWriteRepository,
+        PopulationGenerator generator)
+        : IRequestHandler<InitializePopulationCommand, Unit>
+    {
+        private readonly IPersonWriteRepository _personWriteRepository = personWriteRepository;
+        private readonly PopulationGenerator _generator = generator;
+
+        public async Task<Unit> Handle(
+            InitializePopulationCommand request,
+            CancellationToken cancellationToken)
+        {
+            // 1. Очистить текущую популяцию (если это "reset")
+            await _personWriteRepository.DeleteAllAsync(cancellationToken);
+
+            // 2. Сгенерировать людей
+            var persons = _generator.Generate(
+                request.PeopleCount,
+                request.RandomSeed);
+
+            // 3. Сохранить в БД
+            await _personWriteRepository.AddRangeAsync(persons, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
