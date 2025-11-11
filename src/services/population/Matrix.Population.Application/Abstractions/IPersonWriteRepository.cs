31f8a25049ee1cb90147c87f0cf32d0c77e2720d
Londongrad <Teslagradus@gmail.com>
2025-11-11 23:42:18 +0900

    feat(person-write-repository): implement PersonWriteRepository

---
+++ src/services/population/Matrix.Population.Application/Abstractions/IPersonWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Matrix.Population.Domain.Entities;
+
+namespace Matrix.Population.Application.Abstractions
+{
+    public interface IPersonWriteRepository
+    {
+        Task DeleteAllAsync(CancellationToken cancellationToken = default);
+        Task DeleteAsync(Person person, CancellationToken cancellationToken = default);
+
+        Task AddRangeAsync(IReadOnlyList<Person> persons, CancellationToken cancellationToken = default);
+
+        Task AddAsync(Person person, CancellationToken cancellationToken = default);
+
+        Task UpdateAsync(Person person, CancellationToken cancellationToken = default);
+        Task UpdateRangeAsync(IReadOnlyList<Person> persons, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ src/services/population/Matrix.Population.Infrastructure/Persistence/Repositories/PersonWriteRepository.cs
@@ -0,0 +1,55 @@
+﻿using Matrix.Population.Application.Abstractions;
+using Matrix.Population.Domain.Entities;
+
+namespace Matrix.Population.Infrastructure.Persistence.Repositories
+{
+    public class PersonWriteRepository(PopulationDbContext context) : IPersonWriteRepository
+    {
+        private readonly PopulationDbContext _dbContext = context;
+
+        public async Task AddAsync(Person person, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddRangeAsync(IReadOnlyList<Person> persons, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAllAsync(CancellationToken cancellationToken = default)
+        {
+            _dbContext.Persons.RemoveRange(_dbContext.Persons);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(Person person, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Person person, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateRangeAsync(IReadOnlyList<Person> persons, CancellationToken cancellationToken = default)
+        {
+            const int batchSize = 1_000; // tune if needed
+
+            for (int i = 0; i < persons.Count; i += batchSize)
+            {
+                var batch = persons
+                    .Skip(i)
+                    .Take(batchSize)
+                    .ToList();
+
+                await _dbContext.Persons.AddRangeAsync(batch, cancellationToken);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+
+                // Important: clear change tracker to avoid memory leak
+                _dbContext.ChangeTracker.Clear();
+            }
+        }
+    }
+}
