95ac992c6a01f689b072ac1220816b2b67f49864
Londongrad <Teslagradus@gmail.com>
2025-11-12 20:01:57 +0900

    feat(population-domain): define MaritalStatus enum

---
+++ src/services/population/Matrix.Population.Domain/Enums/MaritalStatus.cs
@@ -0,0 +1,42 @@
+﻿namespace Matrix.Population.Domain.Enums
+{
+    /// <summary>
+    /// Marital status of a person:
+    /// <list type="bullet">
+    ///   <item>
+    ///     <description><see cref="Single"/> – not married</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="Married"/> – currently married</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="Divorced"/> – marriage legally dissolved</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="Widowed"/> – spouse has died</description>
+    ///   </item>
+    /// </list>
+    /// </summary>
+    public enum MaritalStatus
+    {
+        /// <summary>
+        /// Not married.
+        /// </summary>
+        Single = 1,
+
+        /// <summary>
+        /// Currently married.
+        /// </summary>
+        Married = 2,
+
+        /// <summary>
+        /// Marriage legally dissolved.
+        /// </summary>
+        Divorced = 3,
+
+        /// <summary>
+        /// Spouse has died.
+        /// </summary>
+        Widowed = 4
+    }
+}
