34e154131fd5fb5adc4fda883f0909bbd65c8b58
Londongrad <Teslagradus@gmail.com>
2025-11-12 20:02:48 +0900

    feat(population-domain): define Sex enum

---
+++ src/services/population/Matrix.Population.Domain/Enums/Sex.cs
@@ -0,0 +1,26 @@
+﻿namespace Matrix.Population.Domain.Enums
+{
+    /// <summary>
+    /// Sex of a person:
+    /// <list type="bullet">
+    ///   <item>
+    ///     <description><see cref="Male"/> – male sex</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="Female"/> – female sex</description>
+    ///   </item>
+    /// </list>
+    /// </summary>
+    public enum Sex
+    {
+        /// <summary>
+        /// Male sex.
+        /// </summary>
+        Male,
+
+        /// <summary>
+        /// Female sex.
+        /// </summary>
+        Female
+    }
+}
