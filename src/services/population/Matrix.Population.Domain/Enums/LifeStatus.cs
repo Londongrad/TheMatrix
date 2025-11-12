f37aac186854827c65704372f67842b2b6093bf0
Londongrad <Teslagradus@gmail.com>
2025-11-12 20:01:43 +0900

    feat(population-domain): define LifeStatus enum

---
+++ src/services/population/Matrix.Population.Domain/Enums/LifeStatus.cs
@@ -0,0 +1,26 @@
+﻿namespace Matrix.Population.Domain.Enums
+{
+    /// <summary>
+    /// Life status of a person:
+    /// <list type="bullet">
+    ///   <item>
+    ///     <description><see cref="Alive"/> – person is currently alive</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="Deceased"/> – person has died</description>
+    ///   </item>
+    /// </list>
+    /// </summary>
+    public enum LifeStatus
+    {
+        /// <summary>
+        /// Person is currently alive.
+        /// </summary>
+        Alive = 1,
+
+        /// <summary>
+        /// Person has died.
+        /// </summary>
+        Deceased = 2
+    }
+}
