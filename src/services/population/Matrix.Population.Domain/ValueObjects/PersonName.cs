a157ccd00ac635666104c8677bf24725661e78d3
Londongrad <Teslagradus@gmail.com>
2025-11-13 20:22:35 +0900

    feat(population-domain): introduce PersonName value object

---
+++ src/services/population/Matrix.Population.Domain/ValueObjects/PersonName.cs
@@ -0,0 +1,23 @@
+﻿using Matrix.BuildingBlocks.Domain;
+
+namespace Matrix.Population.Domain.ValueObjects
+{
+    public sealed class PersonName
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string? Patronymic { get; }
+
+        public PersonName(string firstName, string lastName, string? patronymic = null)
+        {
+            FirstName = GuardHelper.AgainstNullOrEmpty(firstName, nameof(FirstName));
+            LastName = GuardHelper.AgainstNullOrEmpty(lastName, nameof(LastName));
+            Patronymic = patronymic;
+        }
+
+        public override string ToString()
+            => Patronymic is null
+                ? $"{LastName} {FirstName}"
+                : $"{LastName} {FirstName} {Patronymic}";
+    }
+}
