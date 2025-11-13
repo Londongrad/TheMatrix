f767953119bcf4a4c6fd93294a361adfc43311e6
Londongrad <Teslagradus@gmail.com>
2025-11-13 20:20:03 +0900

    refactor(population-domain): remove Economic logic from Job and add title prop

--- src/services/population/Matrix.Population.Domain/ValueObjects/Job.cs
+++ src/services/population/Matrix.Population.Domain/ValueObjects/Job.cs
@@ -5,19 +5,12 @@ namespace Matrix.Population.Domain.ValueObjects
     public sealed class Job
     {
         public WorkplaceId WorkplaceId { get; }
-        public decimal GrossMonthlySalary { get; }
-        public decimal IncomeTaxRate { get; }
+        public string Title { get; }
 
-        public Job(WorkplaceId workplaceId, decimal grossMonthlySalary, decimal incomeTaxRate)
+        public Job(WorkplaceId workplaceId, string title)
         {
             WorkplaceId = workplaceId;
-            GrossMonthlySalary = GuardHelper.AgainstNegativeNumber(grossMonthlySalary, nameof(GrossMonthlySalary));
-            IncomeTaxRate = GuardHelper.AgainstOutOfRange(incomeTaxRate, 0, 1, nameof(IncomeTaxRate));
-        }
-
-        public MonthlyIncome CalculateMonthlyIncome()
-        {
-            return MonthlyIncome.FromGrossAndTaxRate(GrossMonthlySalary, IncomeTaxRate);
+            Title = GuardHelper.AgainstNullOrEmpty(title, nameof(Title));
         }
     }
 }
