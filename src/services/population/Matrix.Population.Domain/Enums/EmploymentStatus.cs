ac9d79353ca3a69a52b25396e022e33a6db38624
Londongrad <Teslagradus@gmail.com>
2025-11-13 20:08:54 +0900

    docs(population-domain): add XML docs for EmploymentStatus

--- src/services/population/Matrix.Population.Domain/Enums/EmploymentStatus.cs
+++ src/services/population/Matrix.Population.Domain/Enums/EmploymentStatus.cs
@@ -1,10 +1,50 @@
 ﻿namespace Matrix.Population.Domain.Enums
 {
+    /// <summary>
+    /// Employment status of a person:
+    /// <list type="bullet">
+    ///   <item>
+    ///     <description><see cref="Unemployed"/> – not currently employed</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="Employed"/> – has a paid job</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="Student"/> – main activity is studying</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="Retired"/> – no longer in the workforce due to retirement</description>
+    ///   </item>
+    ///   <item>
+    ///     <description><see cref="None"/> – not specified or not applicable</description>
+    ///   </item>
+    /// </list>
+    /// </summary>
     public enum EmploymentStatus
     {
+        /// <summary>
+        /// Not currently employed.
+        /// </summary>
         Unemployed,
+
+        /// <summary>
+        /// Has a paid job.
+        /// </summary>
         Employed,
+
+        /// <summary>
+        /// Main activity is studying.
+        /// </summary>
         Student,
-        Retired
+
+        /// <summary>
+        /// No longer in the workforce due to retirement.
+        /// </summary>
+        Retired,
+
+        /// <summary>
+        /// Not specified or not applicable.
+        /// </summary>
+        None
     }
 }
