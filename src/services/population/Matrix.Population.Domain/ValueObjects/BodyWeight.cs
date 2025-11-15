cdb0dba4eb5bb4756f5de22218fdd83422b04482
Londongrad <Teslagradus@gmail.com>
2025-11-15 15:23:15 +0900

    feat(population-domain): introduce BodyWeight value object

---
+++ src/services/population/Matrix.Population.Domain/ValueObjects/BodyWeight.cs
@@ -0,0 +1,13 @@
+﻿using Matrix.BuildingBlocks.Domain;
+
+namespace Matrix.Population.Domain.ValueObjects
+{
+    public readonly struct BodyWeight(decimal kilograms)
+    {
+        public decimal Kilograms { get; } = GuardHelper.AgainstOutOfRange(kilograms, 2m, 400m, nameof(BodyWeight));
+
+        public static BodyWeight FromKilograms(decimal kilograms) => new(kilograms);
+
+        public override string ToString() => $"{Kilograms:0.#} kg";
+    }
+}
