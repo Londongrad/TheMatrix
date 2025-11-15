8458bf9cbd18fb0ae3d39be5f26efdd5a257d654
Londongrad <Teslagradus@gmail.com>
2025-11-15 15:23:23 +0900

    feat(population-domain): introduce HealthLevel value object

---
+++ src/services/population/Matrix.Population.Domain/ValueObjects/HealthLevel.cs
@@ -0,0 +1,21 @@
+﻿using Matrix.BuildingBlocks.Domain;
+
+namespace Matrix.Population.Domain.ValueObjects
+{
+    public readonly struct HealthLevel(int value)
+    {
+        public int Value { get; } = GuardHelper.AgainstOutOfRange(value, 0, 100, nameof(HealthLevel));
+
+        public static HealthLevel From(int value) => new(value);
+
+        public static HealthLevel Default() => new(100); // здоров как бык
+
+        public HealthLevel WithDelta(int delta)
+        {
+            var newValue = Math.Clamp(Value + delta, 0, 100);
+            return new HealthLevel(newValue);
+        }
+
+        public bool IsDead => Value == 0;
+    }
+}
