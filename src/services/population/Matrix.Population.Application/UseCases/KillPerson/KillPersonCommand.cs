2fd5c189e19ac429f8930d11f73adf15e2815dfb
Londongrad <Teslagradus@gmail.com>
2025-11-15 14:09:13 +0900

    feat(population-application): make KillPersonCommand return PersonDto and rename it

--- src/services/population/Matrix.Population.Application/UseCases/KillPerson/KillPersonCommand.cs
+++ src/services/population/Matrix.Population.Application/UseCases/KillPerson/KillPersonCommand.cs
@@ -1,6 +1,7 @@
-﻿using MediatR;
+﻿using Matrix.Population.Application.DTOs;
+using MediatR;
 
 namespace Matrix.Population.Application.UseCases.KillPerson
 {
-    public record class KillPersonCommand(Guid Id) : IRequest;
+    public record class KillPersonCommand(Guid Id) : IRequest<PersonDto>;
 }
---
+++ src/services/population/Matrix.Population.Application/UseCases/KillPerson/KillPersonCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Matrix.Population.Application.Abstractions;
+using Matrix.Population.Application.DTOs;
+using MediatR;
+
+namespace Matrix.Population.Application.UseCases.KillPerson
+{
+    public class KillPersonCommandHandler(
+        IPersonReadRepository personReadRepository,
+        IPersonWriteRepository personWriteRepository,
+        IMapper mapper) 
+        : IRequestHandler<KillPersonCommand, PersonDto>
+    {
+        public async Task<PersonDto> Handle(KillPersonCommand request, CancellationToken cancellationToken = default)
+        {
+            ArgumentNullException.ThrowIfNull(request, nameof(request));
+
+            var person = await personReadRepository.GetByIdAsync(request.Id, cancellationToken) 
+                ?? throw new KeyNotFoundException($"Person with Id {request.Id} not found.");
+
+            person.Die();
+
+            await personWriteRepository.UpdateAsync(person, cancellationToken);
+
+            return mapper.Map<PersonDto>(person);
+        }
+    }
+}
