3a4b641c3c75cdba8cb2324e84d1b071672d6b4f
Londongrad <Teslagradus@gmail.com>
2025-11-15 14:11:18 +0900

    feat(population-application): implement GetCitizensPageQuery

---
+++ src/services/population/Matrix.Population.Application/UseCases/GetCitizenPage/GetCitizensPageQuery.cs
@@ -0,0 +1,9 @@
+﻿using Matrix.BuildingBlocks.Application.Models;
+using Matrix.Population.Application.DTOs;
+using MediatR;
+
+namespace Matrix.Population.Application.UseCases.GetCitizenPage
+{
+    public sealed record GetCitizensPageQuery(Pagination Pagination)
+    : IRequest<PagedResult<PersonDto>>;
+}
---
+++ src/services/population/Matrix.Population.Application/UseCases/GetCitizenPage/GetCitizensPageQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Matrix.BuildingBlocks.Application.Models;
+using Matrix.Population.Application.Abstractions;
+using Matrix.Population.Application.DTOs;
+using MediatR;
+
+namespace Matrix.Population.Application.UseCases.GetCitizenPage
+{
+    public sealed class GetCitizensPageQueryHandler(
+        IPersonReadRepository personReadRepository,
+        IMapper mapper)
+        : IRequestHandler<GetCitizensPageQuery, PagedResult<PersonDto>>
+    {
+        private readonly IPersonReadRepository _personReadRepository = personReadRepository;
+
+        public async Task<PagedResult<PersonDto>> Handle(
+            GetCitizensPageQuery request,
+            CancellationToken cancellationToken)
+        {
+            var (persons, totalCount) = await _personReadRepository
+                .GetPageAsync(request.Pagination, cancellationToken);
+
+            var dtos = mapper.Map<IReadOnlyCollection<PersonDto>>(persons);
+
+            return new PagedResult<PersonDto>
+            (
+                items: dtos,
+                totalCount: totalCount,
+                pageNumber: request.Pagination.PageNumber,
+                pageSize: request.Pagination.PageSize
+            );
+        }
+    }
+}
