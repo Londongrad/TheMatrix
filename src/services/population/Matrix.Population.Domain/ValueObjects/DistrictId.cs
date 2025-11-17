97e185e2b5eac62433a0df646aebfcdc9718dcbf
Londongrad <Teslagradus@gmail.com>
2025-11-17 18:39:06 +0900

    fix(population-domain): remove duplicate DistrictId state and add From factory

--- src/services/population/Matrix.Population.Domain/ValueObjects/DistrictId.cs
+++ src/services/population/Matrix.Population.Domain/ValueObjects/DistrictId.cs
@@ -2,10 +2,17 @@
 
 namespace Matrix.Population.Domain.ValueObjects
 {
-    public readonly record struct DistrictId(Guid Value)
+    public readonly record struct DistrictId
     {
-        public Guid Value { get; } = GuardHelper.AgainstEmptyGuid(Value, nameof(DistrictId));
+        public Guid Value { get; }
+
+        private DistrictId(Guid value)
+        {
+            Value = GuardHelper.AgainstEmptyGuid(value, nameof(DistrictId));
+        }
 
         public static DistrictId New() => new(Guid.NewGuid());
+
+        public static DistrictId From(Guid value) => new(value);
     }
 }
