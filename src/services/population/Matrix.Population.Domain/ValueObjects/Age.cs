c43291c7752305c8d8608429db3023205bfccd90
Londongrad <Teslagradus@gmail.com>
2025-11-17 18:28:13 +0900

    refactor(population-domain): remove compare methods and change Age from class to record struct

--- src/services/population/Matrix.Population.Domain/ValueObjects/Age.cs
+++ src/services/population/Matrix.Population.Domain/ValueObjects/Age.cs
@@ -3,7 +3,7 @@ using Matrix.BuildingBlocks.Domain.Exceptions;
 
 namespace Matrix.Population.Domain.ValueObjects
 {
-    public sealed class Age : IComparable<Age>, IEquatable<Age>
+    public readonly record struct Age
     {
         public const int MinYears = 0;
         public const int MaxYears = 120;
@@ -19,7 +19,7 @@ namespace Matrix.Population.Domain.ValueObjects
                 nameof(Age));
         }
 
-        public static Age FromYears(int years) => new Age(years);
+        public static Age FromYears(int years) => new(years);
 
         public static Age FromBirthDate(DateOnly birthDate, DateOnly currentDate)
         {
@@ -29,11 +29,8 @@ namespace Matrix.Population.Domain.ValueObjects
                     nameof(currentDate));
 
             var years = currentDate.Year - birthDate.Year;
-
             if (currentDate < birthDate.AddYears(years))
-            {
                 years--;
-            }
 
             return new Age(years);
         }
@@ -49,7 +46,6 @@ namespace Matrix.Population.Domain.ValueObjects
                     nameof(years));
 
             var newYears = Years + years;
-
             if (newYears > MaxYears)
                 throw new DomainValidationException(
                     $"Age cannot exceed {MaxYears} years.",
@@ -57,47 +53,5 @@ namespace Matrix.Population.Domain.ValueObjects
 
             return new Age(newYears);
         }
-
-        #region [ Equality / Comparison ]
-
-        public int CompareTo(Age? other)
-        {
-            if (other is null) return 1;
-            return Years.CompareTo(other.Years);
-        }
-
-        public bool Equals(Age? other)
-        {
-            if (other is null) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Years == other.Years;
-        }
-
-        public override bool Equals(object? obj) =>
-            ReferenceEquals(this, obj) || obj is Age other && Equals(other);
-
-        public override int GetHashCode() => Years;
-
-        public static bool operator ==(Age? left, Age? right) =>
-            Equals(left, right);
-
-        public static bool operator !=(Age? left, Age? right) =>
-            !Equals(left, right);
-
-        public static bool operator <(Age left, Age right) =>
-            left.CompareTo(right) < 0;
-
-        public static bool operator >(Age left, Age right) =>
-            left.CompareTo(right) > 0;
-
-        public static bool operator <=(Age left, Age right) =>
-            left.CompareTo(right) <= 0;
-
-        public static bool operator >=(Age left, Age right) =>
-            left.CompareTo(right) >= 0;
-
-        public override string ToString() => Years.ToString();
-
-        #endregion [ Equality / Comparison ]
     }
 }
