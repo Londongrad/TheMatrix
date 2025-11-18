f88ac681a1ab77b1083a57fe72a65f29e9462385
Londongrad <Teslagradus@gmail.com>
2025-11-18 11:43:57 +0900

    refactor(population-application): extract external PersonDto into common project

---
+++ src/services/population/Matrix.Population.Contracts/Models/PersonDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Matrix.Population.Contracts.Models
+{
+    public sealed record class PersonDto(
+        Guid Id,
+
+        string FullName,
+        string Sex,
+        string BirthDate,
+        string? DeathDate,
+        int Age,
+        string AgeGroup,
+        string LifeStatus,
+        string MaritalStatus,
+        string EducationLevel,
+        int Happiness,
+        string EmploymentStatus,
+        string? JobTitle
+        );
+}
