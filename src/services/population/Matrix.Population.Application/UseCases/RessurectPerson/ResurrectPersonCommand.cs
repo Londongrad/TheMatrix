a26790e742ac9a91b288038522010ad81b96228f
Londongrad <Teslagradus@gmail.com>
2025-11-19 11:49:23 +0900

    feat(population-application): implement ResurrectPerson command

---
+++ src/services/population/Matrix.Population.Application/UseCases/RessurectPerson/ResurrectPersonCommand.cs
@@ -0,0 +1,7 @@
+﻿using Matrix.Population.Contracts.Models;
+using MediatR;
+
+namespace Matrix.Population.Application.UseCases.RessurectPerson
+{
+    public sealed record ResurrectPersonCommand(Guid Id) : IRequest<PersonDto>;
+}
---
+++ src/services/population/Matrix.Population.Application/UseCases/RessurectPerson/ResurrectPersonCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Matrix.Population.Application.Abstractions;
+using Matrix.Population.Application.Mapping;
+using Matrix.Population.Contracts.Models;
+using Matrix.Population.Domain.ValueObjects;
+using MediatR;
+
+namespace Matrix.Population.Application.UseCases.RessurectPerson
+{
+    public sealed class ResurrectPersonCommandHandler(
+        IPersonReadRepository personReadRepository,
+        IPersonWriteRepository personWriteRepository
+        )
+        : IRequestHandler<ResurrectPersonCommand, PersonDto>
+    {
+        private readonly IPersonWriteRepository _personWriteRepository = personWriteRepository;
+        private readonly IPersonReadRepository _personReadRepository = personReadRepository;
+
+        public async Task<PersonDto> Handle(
+            ResurrectPersonCommand request,
+            CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+
+            var person = await _personReadRepository.GetByIdAsync(PersonId.From(request.Id), cancellationToken);
+
+            // TODO: Брать дату извне
+            person.Resurrect(DateOnly.FromDateTime(DateTime.UtcNow));
+
+            await _personWriteRepository.UpdateAsync(person, cancellationToken);
+
+            return person.ToDto();
+        }
+    }
+}
