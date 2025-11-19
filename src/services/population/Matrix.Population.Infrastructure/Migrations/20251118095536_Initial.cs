31d2db027d97c32809fa52f99a7ae3890afdcfcd
Londongrad <Teslagradus@gmail.com>
2025-11-19 11:48:02 +0900

    chore(population-migrations): add Initial migrations

---
+++ src/services/population/Matrix.Population.Infrastructure/Migrations/20251118095536_Initial.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matrix.Population.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    HouseholdId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DistrictId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    MiddleName = table.Column<string>(type: "text", nullable: true),
+                    LifeStatus = table.Column<string>(type: "text", nullable: false),
+                    Sex = table.Column<string>(type: "text", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "date", nullable: false),
+                    MaritalStatus = table.Column<string>(type: "text", nullable: false),
+                    EducationLevel = table.Column<string>(type: "text", nullable: false),
+                    Health = table.Column<int>(type: "integer", nullable: false),
+                    WeightKg = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    Happiness = table.Column<int>(type: "integer", nullable: false),
+                    EmploymentStatus = table.Column<string>(type: "text", nullable: false),
+                    WorkplaceId = table.Column<Guid>(type: "uuid", nullable: true),
+                    JobTitle = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Optimism = table.Column<int>(type: "integer", nullable: false),
+                    Discipline = table.Column<int>(type: "integer", nullable: false),
+                    RiskTolerance = table.Column<int>(type: "integer", nullable: false),
+                    Sociability = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ src/services/population/Matrix.Population.Infrastructure/Migrations/20251120093225_AddLifeSpanProp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matrix.Population.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLifeSpanProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeathDate",
+                table: "Persons",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeathDate",
+                table: "Persons");
+        }
+    }
+}
