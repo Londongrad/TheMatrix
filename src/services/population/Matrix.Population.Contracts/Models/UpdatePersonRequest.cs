de0c8d60eb1d88e9bb329662707db94708c3f144
Londongrad <Teslagradus@gmail.com>
2025-11-20 12:17:12 +0900

    feat(population-contracts): add UpdatePersonRequest DTO for API

---
+++ src/services/population/Matrix.Population.Contracts/Models/UpdatePersonRequest.cs
@@ -0,0 +1,13 @@
+﻿using Matrix.Population.Domain.Enums;
+
+namespace Matrix.Population.Contracts.Models
+{
+    public sealed record UpdatePersonRequest(
+        string? FullName,
+        MaritalStatus? MaritalStatus,
+        EducationLevel? EducationLevel,
+        EmploymentStatus? EmploymentStatus,
+        string? JobTitle,
+        int? Happiness
+    );
+}
