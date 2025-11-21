2273757378b241626c084d1b577e80afcc499587
Londongrad <Teslagradus@gmail.com>
2025-11-21 14:54:26 +0900

    chore(identity-application): add service registration extension for identity application

---
+++ src/services/identity/Matrix.Identity.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Matrix.Identity.Application.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Matrix.Identity.Application
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(IUserRepository).Assembly);
+            });
+        }
+    }
+}
