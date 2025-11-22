1177b88447660ded7c12345b5cec4c9362437641
Londongrad <Teslagradus@gmail.com>
2025-11-22 15:00:10 +0900

    feat(identity-application): add shared AccessTokenModel for auth commands

---
+++ src/services/identity/Matrix.Identity.Application/UseCases/AccessTokenModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Matrix.Identity.Application.UseCases
+{
+    public sealed class AccessTokenModel
+    {
+        public required string Token { get; init; }
+
+        public string TokenType { get; init; } = "Bearer";
+
+        /// <summary>
+        /// Время жизни токена в секундах.
+        /// </summary>
+        public required int ExpiresInSeconds { get; init; }
+    }
+}
