c3a0fe7d8158413195fe4fc85902940656e4e2dd
Londongrad <Teslagradus@gmail.com>
2025-11-26 16:07:21 +0900

    feat(building-blocks-application): add base MatrixApplicationException

---
+++ src/building-blocks/Matrix.BuildingBlocks.Application/Exceptions/MatrixApplicationException.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Matrix.BuildingBlocks.Application.Exceptions
+{
+    /// <summary>
+    /// Base exception type for application layer errors in Matrix.
+    /// </summary>
+    public abstract class MatrixApplicationException(
+        string code,
+        string message,
+        HttpStatusCode statusCode = HttpStatusCode.BadRequest,
+        IDictionary<string, string[]>? errors = null) 
+        : Exception(message)
+    {
+        public string Code { get; } = code;
+        public HttpStatusCode StatusCode { get; } = statusCode;
+        public IDictionary<string, string[]>? Errors { get; } = errors;
+    }
+}
