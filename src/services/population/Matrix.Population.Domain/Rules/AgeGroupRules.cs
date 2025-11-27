26d493646da862690ef8a706e596d313fe88fc9d
Londongrad <Teslagradus@gmail.com>
2025-11-27 16:29:34 +0900

    refactor(population-domain): adjust AgeGroupRules for Youth < 18

--- src/services/population/Matrix.Population.Domain/Rules/AgeGroupRules.cs
+++ src/services/population/Matrix.Population.Domain/Rules/AgeGroupRules.cs
@@ -8,7 +8,7 @@ namespace Matrix.Population.Domain.Rules
         public static AgeGroup GetAgeGroup(Age age)
         {
             if (age.Years < 7) return AgeGroup.Child;
-            if (age.Years < 23) return AgeGroup.Youth;
+            if (age.Years < 17) return AgeGroup.Youth;
             if (age.Years < 66) return AgeGroup.Adult;
             return AgeGroup.Senior;
         }
