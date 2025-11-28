cadcea6d215a3b5de83549de0b50075e6e71edad
Londongrad <Teslagradus@gmail.com>
2025-11-28 16:34:14 +0900

    feat(population-domain): add PersonHappinessDeltas constants (prep for refactor)

---
+++ src/services/population/Matrix.Population.Domain/Rules/PersonHappinessDeltas.cs
@@ -0,0 +1,20 @@
+﻿namespace Matrix.Population.Domain.Rules
+{
+    /// <summary>
+    /// Базовые изменения уровня счастья при разных жизненных событиях.
+    /// Это "сырые" дельты, которые потом модифицируются Personality.
+    /// </summary>
+    public static class PersonHappinessDeltas
+    {
+        public const int OnJobAssigned = +10;
+        public const int OnFired = -10;
+
+        public const int OnRetired = +5;
+        public const int OnStatusStudent = +5;
+        public const int OnStatusNone = -5;
+
+        public const int OnMarry = +15;
+        public const int OnDivorce = -15;
+        public const int OnWidow = -30;
+    }
+}
