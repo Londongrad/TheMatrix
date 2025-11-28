c8d5f926a25895502d0f94eec767a512e33bb1e8
Londongrad <Teslagradus@gmail.com>
2025-11-28 16:37:42 +0900

    feat(population-domain): add EducationInfo value object with validated level progression

---
+++ src/services/population/Matrix.Population.Domain/ValueObjects/EducationInfo.cs
@@ -0,0 +1,28 @@
+﻿using Matrix.BuildingBlocks.Domain;
+using Matrix.Population.Domain.Enums;
+using Matrix.Population.Domain.Rules;
+
+namespace Matrix.Population.Domain.ValueObjects
+{
+    public sealed class EducationInfo
+    {
+        public EducationLevel Level { get; }
+
+        private EducationInfo(EducationLevel level)
+        {
+            Level = GuardHelper.AgainstInvalidEnum(level, nameof(level));
+        }
+
+        public static EducationInfo FromLevel(EducationLevel level) =>
+            new(level);
+
+        public static EducationInfo None() =>
+            new(EducationLevel.None);
+
+        public EducationInfo GraduateTo(EducationLevel newLevel)
+        {
+            EducationRules.ValidateTransition(Level, newLevel);
+            return new EducationInfo(newLevel);
+        }
+    }
+}
