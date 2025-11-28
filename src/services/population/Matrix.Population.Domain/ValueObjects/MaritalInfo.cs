42cbec519a46f75f6915bc917bc753a05e956eaf
Londongrad <Teslagradus@gmail.com>
2025-11-28 16:39:42 +0900

    feat(population-domain): add MaritalInfo value object for marital status and partner link

---
+++ src/services/population/Matrix.Population.Domain/ValueObjects/MaritalInfo.cs
@@ -0,0 +1,29 @@
+﻿using Matrix.BuildingBlocks.Domain;
+using Matrix.Population.Domain.Enums;
+using Matrix.Population.Domain.Rules;
+
+namespace Matrix.Population.Domain.ValueObjects
+{
+    public sealed class MaritalInfo
+    {
+        public MaritalStatus Status { get; }
+        public PersonId? SpouseId { get; }
+
+        private MaritalInfo(MaritalStatus status, PersonId? spouseId)
+        {
+            Status = GuardHelper.AgainstInvalidEnum(status, nameof(status));
+            SpouseId = spouseId;
+
+            MaritalRules.ValidateStatusCombination(Status, SpouseId);
+        }
+
+        public static MaritalInfo Single() =>
+            new(MaritalStatus.Single, null);
+
+        public static MaritalInfo Widowed() =>
+            new(MaritalStatus.Widowed, null);
+
+        public static MaritalInfo MarriedWith(PersonId spouseId) =>
+            new(MaritalStatus.Married, spouseId);
+    }
+}
