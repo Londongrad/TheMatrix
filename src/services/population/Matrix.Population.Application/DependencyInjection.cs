043d903a1c4d2616f6c69f0a7b92ecfb0a060898
Londongrad <Teslagradus@gmail.com>
2025-12-02 11:52:50 +0900

    chore(population-application): wire up MediatR logging/validation behaviors and validation exception factory

--- src/services/population/Matrix.Population.Application/DependencyInjection.cs
+++ src/services/population/Matrix.Population.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Matrix.Population.Application.Abstractions;
+﻿using Matrix.BuildingBlocks.Application.Abstractions;
+using Matrix.BuildingBlocks.Application.Behaviors;
+using Matrix.Population.Application.Errors;
 using Matrix.Population.Domain.Services;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Matrix.Population.Application
@@ -8,12 +11,20 @@ namespace Matrix.Population.Application
     {
         public static void AddApplication(this IServiceCollection services)
         {
+            var assembly = typeof(DependencyInjection).Assembly;
+
             services.AddSingleton<PopulationGenerator>();
 
             services.AddMediatR(cfg =>
             {
-                cfg.RegisterServicesFromAssembly(typeof(IPersonReadRepository).Assembly);
+                cfg.RegisterServicesFromAssembly(assembly);
             });
+
+            services.AddScoped<IValidationExceptionFactory, PopulationValidationErrorFactory>();
+
+            // Behaviors (используем общие из BuildingBlocks)
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         }
     }
 }
