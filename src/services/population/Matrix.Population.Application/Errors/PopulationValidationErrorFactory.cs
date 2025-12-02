d846828466c9d3b227d9739b49ec43c37cbef9e6
Londongrad <Teslagradus@gmail.com>
2025-12-02 11:50:46 +0900

    feat(population-application): add validation exception factory

---
+++ src/services/population/Matrix.Population.Application/Errors/PopulationValidationErrorFactory.cs
@@ -0,0 +1,22 @@
+﻿using Matrix.BuildingBlocks.Application.Abstractions;
+using Matrix.BuildingBlocks.Application.Enums;
+using Matrix.BuildingBlocks.Application.Exceptions;
+
+namespace Matrix.Population.Application.Errors
+{
+    public sealed class PopulationValidationErrorFactory : IValidationExceptionFactory
+    {
+        public MatrixApplicationException Create(
+            Type requestType,
+            IReadOnlyDictionary<string, string[]> errors)
+        {
+            var code = $"Population.{requestType.Name}.ValidationFailed";
+            
+            return new MatrixApplicationException(
+                code,
+                "One or more validation errors occurred.",
+                ApplicationErrorType.Validation,
+                errors);
+        }
+    }
+}
