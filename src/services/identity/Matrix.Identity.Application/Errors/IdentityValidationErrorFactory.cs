18161bd3705daadaa3520058c4314e0c945c4a5f
Londongrad <Teslagradus@gmail.com>
2025-12-03 14:38:56 +0900

    feat(identity-application): implement IValidationExceptionFactory

---
+++ src/services/identity/Matrix.Identity.Application/Errors/IdentityValidationErrorFactory.cs
@@ -0,0 +1,22 @@
+﻿using Matrix.BuildingBlocks.Application.Abstractions;
+using Matrix.BuildingBlocks.Application.Enums;
+using Matrix.BuildingBlocks.Application.Exceptions;
+
+namespace Matrix.Identity.Application.Errors
+{
+    public sealed class IdentityValidationErrorFactory : IValidationExceptionFactory
+    {
+        public MatrixApplicationException Create(
+            Type requestType,
+            IReadOnlyDictionary<string, string[]> errors)
+        {
+            var code = $"Identity.{requestType.Name}.ValidationFailed";
+
+            return new MatrixApplicationException(
+                code: code,
+                message: "One or more validation errors occurred.",
+                errorType: ApplicationErrorType.Validation,
+                errors: errors);
+        }
+    }
+}
