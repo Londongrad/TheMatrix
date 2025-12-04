e389f61d30942eb102508c769f662c47791179d2
Londongrad <Teslagradus@gmail.com>
2025-12-04 20:36:41 +0900

    feat(identity-application): implement RevokeAllUserSessions

---
+++ src/services/identity/Matrix.Identity.Application/UseCases/Sessions/RevokeAllUserSessions/RevokeAllUserSessionsCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Matrix.Identity.Application.UseCases.Sessions.RevokeAllUserSessions
+{
+    public sealed record RevokeAllUserSessionsCommand(Guid UserId) : IRequest;
+}
---
+++ src/services/identity/Matrix.Identity.Application/UseCases/Sessions/RevokeAllUserSessions/RevokeAllUserSessionsCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Matrix.Identity.Application.Abstractions;
+using Matrix.Identity.Application.Errors;
+using MediatR;
+
+namespace Matrix.Identity.Application.UseCases.Sessions.RevokeAllUserSessions
+{
+    public sealed class RevokeAllUserSessionsCommandHandler(
+        IUserRepository userRepository)
+        : IRequestHandler<RevokeAllUserSessionsCommand>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+
+        public async Task Handle(
+            RevokeAllUserSessionsCommand request,
+            CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken)
+                ?? throw ApplicationErrorsFactory.UserNotFound(request.UserId);
+
+            user.RevokeAllRefreshTokens();
+
+            await _userRepository.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
