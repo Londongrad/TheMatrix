f1ed368843600262f0d00a2ff5241b1a53642f07
Londongrad <Teslagradus@gmail.com>
2025-12-04 20:34:18 +0900

    feat(identity-application): implement ChangeAvatar command/handler/validator

---
+++ src/services/identity/Matrix.Identity.Application/UseCases/Account/ChangeAvatar/ChangeAvatarCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Matrix.Identity.Application.UseCases.Account.ChangeAvatar
+{
+    public sealed record ChangeAvatarCommand(
+        Guid UserId,
+        string? AvatarUrl
+    ) : IRequest;
+}
---
+++ src/services/identity/Matrix.Identity.Application/UseCases/Account/ChangeAvatar/ChangeAvatarCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Matrix.Identity.Application.Abstractions;
+using Matrix.Identity.Application.Errors;
+using MediatR;
+
+namespace Matrix.Identity.Application.UseCases.Account.ChangeAvatar
+{
+    public sealed class ChangeAvatarCommandHandler(
+        IUserRepository userRepository)
+        : IRequestHandler<ChangeAvatarCommand>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+
+        public async Task Handle(
+            ChangeAvatarCommand request,
+            CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken)
+                ?? throw ApplicationErrorsFactory.UserNotFound(request.UserId);
+
+            user.ChangeAvatar(request.AvatarUrl);
+
+            await _userRepository.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
