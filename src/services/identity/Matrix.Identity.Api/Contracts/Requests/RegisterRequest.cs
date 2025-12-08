f0997da5da27e6e572f898cdef8d246345c0a8c2
Londongrad <Teslagradus@gmail.com>
2025-12-08 09:49:50 +0900

    chore(identity-api): remove data annotations from RegisterRequest

--- src/services/identity/Matrix.Identity.Api/Contracts/Requests/RegisterRequest.cs
+++ src/services/identity/Matrix.Identity.Api/Contracts/Requests/RegisterRequest.cs
@@ -1,18 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Matrix.Identity.Api.Contracts.Requests
 {
     public sealed class RegisterRequest
     {
-        [Required] [EmailAddress] public string Email { get; set; } = null!;
-
-        [Required]
-        [MinLength(3)]
-        [MaxLength(32)]
-        public string Username { get; set; } = null!;
-
-        [Required] [MinLength(6)] public string Password { get; set; } = null!;
-
-        [Required] [Compare(nameof(Password))] public string ConfirmPassword { get; set; } = null!;
+        public required string Email { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
     }
 }
