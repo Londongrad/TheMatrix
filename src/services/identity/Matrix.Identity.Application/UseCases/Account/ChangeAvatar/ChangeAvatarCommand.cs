e9b73b84fa5426a60f9fb5b235fe07a16dc208c1
Londongrad <Teslagradus@gmail.com>
2025-12-08 09:37:51 +0900

    refactor(identity-application): remove ChangeAvatarCommand (URL-based)

--- src/services/identity/Matrix.Identity.Application/UseCases/Account/ChangeAvatar/ChangeAvatarCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Matrix.Identity.Application.UseCases.Account.ChangeAvatar
-{
-    public sealed record ChangeAvatarCommand(
-        Guid UserId,
-        string? AvatarUrl
-    ) : IRequest;
-}
