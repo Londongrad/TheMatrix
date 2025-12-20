da0acb1b68e6a07a1e0b605d27e235a16af8cc39
Londongrad <Teslagradus@gmail.com>
2025-12-20 12:35:47 +0900

    feat(identity-domain): add system role name constants and PermissionEffect enum

---
+++ src/services/identity/Matrix.Identity.Domain/Authorization/SystemRoleNames.cs
@@ -0,0 +1,7 @@
+namespace Matrix.Identity.Domain.Authorization
+{
+    public static class SystemRoleNames
+    {
+        public const string Admin = "Admin";
+    }
+}
---
+++ src/services/identity/Matrix.Identity.Domain/Enums/PermissionEffect.cs
@@ -0,0 +1,8 @@
+namespace Matrix.Identity.Domain.Enums
+{
+    public enum PermissionEffect : byte
+    {
+        Allow = 1,
+        Deny = 2
+    }
+}
