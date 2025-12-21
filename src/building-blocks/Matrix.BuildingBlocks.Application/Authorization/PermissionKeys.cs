072cc7941dfa31423fd3510eca69aab72c3b9cd6
Londongrad <Teslagradus@gmail.com>
2025-12-21 10:50:53 +0900

    chore(building-blocks-application): add PopulationPeopleCreate permission key

--- src/building-blocks/Matrix.BuildingBlocks.Application/Authorization/PermissionKeys.cs
+++ src/building-blocks/Matrix.BuildingBlocks.Application/Authorization/PermissionKeys.cs
@@ -4,6 +4,7 @@ namespace Matrix.BuildingBlocks.Application.Authorization
     {
         public const string IdentityUsersRead = "identity.users.read";
 
+        public const string PopulationPeopleCreate = "population.people.create";
         public const string PopulationPeopleRead = "population.people.read";
         public const string PopulationPeopleKill = "population.people.kill";
         public const string PopulationPeopleResurrect = "population.people.resurrect";
