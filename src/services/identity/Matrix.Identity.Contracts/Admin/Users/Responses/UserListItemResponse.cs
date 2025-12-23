3de62c22590fd714c0b09939128ee4959512fa29
Londongrad <Teslagradus@gmail.com>
2025-12-23 11:21:46 +0900

    feat(identity-contracts): add UserListItemResponse

---
+++ src/services/identity/Matrix.Identity.Contracts/Admin/Users/Responses/UserListItemResponse.cs
@@ -0,0 +1,13 @@
+namespace Matrix.Identity.Contracts.Admin.Users.Responses
+{
+    public sealed class UserListItemResponse
+    {
+        public Guid Id { get; init; }
+        public string? AvatarUrl { get; init; }
+        public required string Email { get; init; }
+        public required string Username { get; init; }
+        public bool IsEmailConfirmed { get; init; }
+        public bool IsLocked { get; init; }
+        public DateTime CreatedAtUtc { get; init; }
+    }
+}
