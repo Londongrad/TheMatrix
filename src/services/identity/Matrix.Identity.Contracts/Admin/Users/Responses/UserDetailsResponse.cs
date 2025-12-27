3f317f35d36cc498e8c99cedfac03ade84a61278
Londongrad <Teslagradus@gmail.com>
2025-12-27 11:01:13 +0900

    refactor(identity-contracts): change AvatarUrl to settable property

--- src/services/identity/Matrix.Identity.Contracts/Admin/Users/Responses/UserDetailsResponse.cs
+++ src/services/identity/Matrix.Identity.Contracts/Admin/Users/Responses/UserDetailsResponse.cs
@@ -3,7 +3,7 @@ namespace Matrix.Identity.Contracts.Admin.Users.Responses
     public sealed class UserDetailsResponse
     {
         public Guid Id { get; init; }
-        public string? AvatarUrl { get; init; }
+        public string? AvatarUrl { get; set; }
         public string Username { get; init; } = string.Empty;
         public string Email { get; init; } = string.Empty;
         public bool IsEmailConfirmed { get; init; }
--- src/services/identity/Matrix.Identity.Contracts/Admin/Users/Responses/UserListItemResponse.cs
+++ src/services/identity/Matrix.Identity.Contracts/Admin/Users/Responses/UserListItemResponse.cs
@@ -3,7 +3,7 @@ namespace Matrix.Identity.Contracts.Admin.Users.Responses
     public sealed class UserListItemResponse
     {
         public Guid Id { get; init; }
-        public string? AvatarUrl { get; init; }
+        public string? AvatarUrl { get; set; }
         public required string Email { get; init; }
         public required string Username { get; init; }
         public bool IsEmailConfirmed { get; init; }
