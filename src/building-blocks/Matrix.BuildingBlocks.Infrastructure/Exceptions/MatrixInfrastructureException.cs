c13dc552dbcb6edd06a69edfe9136c60ea623d72
Londongrad <Teslagradus@gmail.com>
2025-12-31 10:38:49 +0900

    chore(identity-infrastructure): add MatrixInfrastructureException

---
+++ src/building-blocks/Matrix.BuildingBlocks.Infrastructure/Exceptions/MatrixInfrastructureException.cs
@@ -0,0 +1,17 @@
+using Matrix.BuildingBlocks.Application.Enums;
+
+namespace Matrix.BuildingBlocks.Infrastructure.Exceptions
+{
+    public sealed class MatrixInfrastructureException(
+        string code,
+        string message,
+        ApplicationErrorType errorType = ApplicationErrorType.Unknown,
+        IReadOnlyDictionary<string, string[]>? details = null,
+        Exception? innerException = null)
+        : Exception(message, innerException)
+    {
+        public string Code { get; } = string.IsNullOrWhiteSpace(code) ? "Infrastructure.Error" : code;
+        public ApplicationErrorType ErrorType { get; } = errorType;
+        public IReadOnlyDictionary<string, string[]>? Details { get; } = details;
+    }
+}
