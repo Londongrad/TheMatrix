5c9c4d52a595c326bb0417466464110503bd5060
Londongrad <Teslagradus@gmail.com>
2026-01-09 22:37:59 +0900

    chore(population-api): use ExceptionHandlingMiddleware

--- src/services/population/Matrix.Population.Api/Configurations/MiddlewareConfiguration.cs
+++ src/services/population/Matrix.Population.Api/Configurations/MiddlewareConfiguration.cs
@@ -1,9 +1,12 @@
+using Matrix.BuildingBlocks.Api.Middleware;
+
 namespace Matrix.Population.Api.Configurations
 {
     public static class MiddlewareConfiguration
     {
         public static void ConfigureApplicationMiddleware(this WebApplication app)
         {
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
             app.ConfigureControllers();
             app.UseSecurityPipeline();
         }
