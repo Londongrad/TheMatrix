34f2317d53c42b3ee44a87a589eff2af12f8c841
Londongrad <teslagradus@gmail.com>
2026-01-11 14:09:25 +0900

    fix(gateway): unify permissions version cache key and TTL
    - Expose PermissionVersion TTL

--- src/gateways/Matrix.ApiGateway/Consumers/UserSecurityStateChangedConsumer.cs
+++ src/gateways/Matrix.ApiGateway/Consumers/UserSecurityStateChangedConsumer.cs
@@ -1,25 +1,30 @@
 using System.Globalization;
 using MassTransit;
+using Matrix.ApiGateway.Authorization.PermissionsVersion;
+using Matrix.ApiGateway.Authorization.PermissionsVersion.Options;
 using Matrix.Identity.Contracts.Internal.Events;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
 
 namespace Matrix.ApiGateway.Consumers
 {
-    public sealed class UserSecurityStateChangedConsumer(IDistributedCache cache)
+    public sealed class UserSecurityStateChangedConsumer(
+        IDistributedCache cache,
+        IOptions<PermissionsVersionOptions> options)
         : IConsumer<UserSecurityStateChangedV1>
     {
         public Task Consume(ConsumeContext<UserSecurityStateChangedV1> context)
         {
             UserSecurityStateChangedV1 msg = context.Message;
 
-            string key = $"pv:{msg.UserId}";
+            string key = PermissionsVersionCacheKeys.ForUser(msg.UserId);
 
             return cache.SetStringAsync(
                 key: key,
                 value: msg.PermissionsVersion.ToString(CultureInfo.InvariantCulture),
                 options: new DistributedCacheEntryOptions
                 {
-                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30)
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(options.Value.CacheTtlSeconds)
                 },
                 token: context.CancellationToken);
         }
