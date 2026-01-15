506cdaef7c67fe64f0226ca59b9dc7d4f7635ec3
Londongrad <teslagradus@gmail.com>
2026-01-15 12:01:48 +0900

    feat(citycore-domain): add CityId

---
+++ src/services/citycore/Matrix.CityCore.Domain/Time/CityId.cs
@@ -0,0 +1,24 @@
+﻿using Matrix.BuildingBlocks.Domain;
+
+namespace Matrix.CityCore.Domain.Time
+{
+    /// <summary>
+    ///     Strongly-typed identifier for a city.
+    /// </summary>
+    public readonly record struct CityId(Guid Value)
+    {
+        public Guid Value { get; } = GuardHelper.AgainstEmptyGuid(
+            id: Value,
+            propertyName: nameof(CityId));
+
+        public static CityId New()
+        {
+            return new CityId(Guid.NewGuid());
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
