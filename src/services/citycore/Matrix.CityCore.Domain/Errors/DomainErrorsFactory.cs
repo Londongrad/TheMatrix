5fb6bdc6ec9a33b68d864e62e26c09e0c8e32e93
Londongrad <teslagradus@gmail.com>
2026-01-15 12:01:28 +0900

    chore(citycore-domain): add DomainErrorsFactory

---
+++ src/services/citycore/Matrix.CityCore.Domain/Errors/DomainErrorsFactory.cs
@@ -0,0 +1,39 @@
+﻿using Matrix.BuildingBlocks.Domain.Exceptions;
+
+namespace Matrix.CityCore.Domain.Errors
+{
+    public static class DomainErrorsFactory
+    {
+        public static DomainException SimTimeMustBeUtc(
+            DateTimeOffset value,
+            string? propertyName = null)
+        {
+            return new DomainException(
+                code: "CityCore.SimTime.NotUtc",
+                message: "SimTime must be in UTC (Offset=00:00).",
+                propertyName: propertyName);
+        }
+
+        public static DomainException SimSpeedMultiplierOutOfRange(
+            decimal value,
+            decimal min,
+            decimal max,
+            string? propertyName = null)
+        {
+            return new DomainException(
+                code: "CityCore.SimSpeed.Multiplier.OutOfRange",
+                message: $"SimSpeed multiplier must be in range [{min}; {max}].",
+                propertyName: propertyName);
+        }
+
+        public static DomainException SimSpeedRealDeltaMustBePositive(
+            TimeSpan value,
+            string? propertyName = null)
+        {
+            return new DomainException(
+                code: "CityCore.SimSpeed.RealDelta.NotPositive",
+                message: "realDelta must be positive.",
+                propertyName: propertyName);
+        }
+    }
+}
