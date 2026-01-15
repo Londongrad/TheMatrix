d79b878f04382e140249e2b23a0ca230aa8ec7be
Londongrad <teslagradus@gmail.com>
2026-01-15 16:02:06 +0900

    feat(citycore-application): add bootstrap-city use case

---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/BootstrapCity/BootstrapCityCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Matrix.CityCore.Application.UseCases.BootstrapCity
+{
+    public sealed record BootstrapCityCommand(
+        Guid CityId,
+        DateTimeOffset StartSimTimeUtc) : IRequest;
+}
---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/BootstrapCity/BootstrapCityCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Matrix.BuildingBlocks.Application.Abstractions;
+using Matrix.CityCore.Application.Abstractions.Persistence;
+using Matrix.CityCore.Domain.Time;
+using MediatR;
+
+namespace Matrix.CityCore.Application.UseCases.BootstrapCity
+{
+    public sealed class BootstrapCityCommandHandler(
+        ISimulationClockRepository repository,
+        IUnitOfWork unitOfWork) : IRequestHandler<BootstrapCityCommand>
+    {
+        public async Task Handle(
+            BootstrapCityCommand request,
+            CancellationToken cancellationToken)
+        {
+            var cityId = new CityId(request.CityId);
+
+            SimulationClock? existing = await repository.GetByCityIdAsync(
+                cityId: cityId,
+                cancellationToken: cancellationToken);
+
+            if (existing is not null)
+                return;
+
+            var clock = SimulationClock.Create(
+                cityId: cityId,
+                startTime: SimTime.FromUtc(request.StartSimTimeUtc),
+                speed: SimSpeed.RealTime());
+
+            await repository.AddAsync(
+                clock: clock,
+                cancellationToken: cancellationToken);
+
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
