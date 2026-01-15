30be4789f3945f547dce0cfb67490b6a2f25965a
Londongrad <teslagradus@gmail.com>
2026-01-15 12:00:57 +0900

    chore(citycore-domain): add basic domain primitives (AggregationRoot, Entity)

---
+++ src/services/citycore/Matrix.CityCore.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,26 @@
+﻿using Matrix.CityCore.Domain.Events;
+
+namespace Matrix.CityCore.Domain.Common
+{
+    /// <summary>
+    ///     Base type for aggregate roots with domain events support.
+    /// </summary>
+    public abstract class AggregateRoot<TId>(TId id)
+        : Entity<TId>(id)
+        where TId : notnull
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents;
+
+        protected void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
---
+++ src/services/citycore/Matrix.CityCore.Domain/Common/Entity.cs
@@ -0,0 +1,24 @@
+﻿namespace Matrix.CityCore.Domain.Common
+{
+    /// <summary>
+    ///     Base type for domain entities.
+    /// </summary>
+    public abstract class Entity<TId>(TId id)
+        where TId : notnull
+    {
+        public TId Id { get; } = id;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Entity<TId> other &&
+                   EqualityComparer<TId>.Default.Equals(
+                       x: Id,
+                       y: other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
