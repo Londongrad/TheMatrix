dbb261d9dfc4e44b6a1e829437105f99c54caa7d
Londongrad <teslagradus@gmail.com>
2026-01-16 10:03:48 +0900

    feat(citycore-application): add jump-clock use case

---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/JumpClock/JumpClockCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Matrix.CityCore.Application.UseCases.JumpClock
+{
+    public sealed record JumpClockCommand(
+        Guid CityId,
+        DateTimeOffset NewSimTimeUtc) : IRequest<bool>;
+}
---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/JumpClock/JumpClockCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Matrix.BuildingBlocks.Application.Abstractions;
+using Matrix.CityCore.Application.Abstractions.Persistence;
+using Matrix.CityCore.Domain.Time;
+using MediatR;
+
+namespace Matrix.CityCore.Application.UseCases.JumpClock
+{
+    public sealed class JumpClockCommandHandler(
+        ISimulationClockRepository repository,
+        IUnitOfWork unitOfWork) : IRequestHandler<JumpClockCommand, bool>
+    {
+        public async Task<bool> Handle(
+            JumpClockCommand request,
+            CancellationToken cancellationToken)
+        {
+            SimulationClock? clock = await repository.GetByCityIdAsync(
+                cityId: new CityId(request.CityId),
+                cancellationToken: cancellationToken);
+            if (clock is null)
+                return false;
+
+            clock.JumpTo(SimTime.FromUtc(request.NewSimTimeUtc));
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
