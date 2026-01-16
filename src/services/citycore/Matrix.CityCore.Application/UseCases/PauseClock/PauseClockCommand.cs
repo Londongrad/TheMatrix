165b420262c3393a6611a65e5b46cebdede19c1d
Londongrad <teslagradus@gmail.com>
2026-01-16 10:03:17 +0900

    feat(citycore-application): add pause-clock use case

---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/PauseClock/PauseClockCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Matrix.CityCore.Application.UseCases.PauseClock
+{
+    public sealed record PauseClockCommand(Guid CityId) : IRequest<bool>;
+}
---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/PauseClock/PauseClockCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Matrix.BuildingBlocks.Application.Abstractions;
+using Matrix.CityCore.Application.Abstractions.Persistence;
+using Matrix.CityCore.Domain.Time;
+using MediatR;
+
+namespace Matrix.CityCore.Application.UseCases.PauseClock
+{
+    public sealed class PauseClockCommandHandler(
+        ISimulationClockRepository repository,
+        IUnitOfWork unitOfWork) : IRequestHandler<PauseClockCommand, bool>
+    {
+        public async Task<bool> Handle(
+            PauseClockCommand request,
+            CancellationToken cancellationToken)
+        {
+            SimulationClock? clock = await repository.GetByCityIdAsync(
+                cityId: new CityId(request.CityId),
+                cancellationToken: cancellationToken);
+
+            if (clock is null)
+                return false;
+
+            clock.Pause();
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
