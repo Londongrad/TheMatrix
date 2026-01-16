f8aa27284bce92cb5daa8349f0a9ffb37c9fb53c
Londongrad <teslagradus@gmail.com>
2026-01-16 10:03:39 +0900

    feat(citycore-application): add set-clock-speed use case

---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/SetClockSpeed/SetClockSpeedCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Matrix.CityCore.Application.UseCases.SetClockSpeed
+{
+    public sealed record SetClockSpeedCommand(
+        Guid CityId,
+        decimal Multiplier) : IRequest<bool>;
+}
---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/SetClockSpeed/SetClockSpeedCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Matrix.BuildingBlocks.Application.Abstractions;
+using Matrix.CityCore.Application.Abstractions.Persistence;
+using Matrix.CityCore.Domain.Time;
+using MediatR;
+
+namespace Matrix.CityCore.Application.UseCases.SetClockSpeed
+{
+    public sealed class SetClockSpeedCommandHandler(
+        ISimulationClockRepository repository,
+        IUnitOfWork unitOfWork) : IRequestHandler<SetClockSpeedCommand, bool>
+    {
+        public async Task<bool> Handle(
+            SetClockSpeedCommand request,
+            CancellationToken cancellationToken)
+        {
+            SimulationClock? clock = await repository.GetByCityIdAsync(
+                cityId: new CityId(request.CityId),
+                cancellationToken: cancellationToken);
+
+            if (clock is null)
+                return false;
+
+            clock.SetSpeed(SimSpeed.From(request.Multiplier));
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
