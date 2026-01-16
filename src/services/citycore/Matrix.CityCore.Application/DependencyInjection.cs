f5cff9d834bf988ad5580591c90a86f5d5760a23
Londongrad <teslagradus@gmail.com>
2026-01-16 10:04:22 +0900

    feat(citycore-application): wire MediatR application module

---
+++ src/services/citycore/Matrix.CityCore.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Matrix.CityCore.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCityCoreApplication(this IServiceCollection services)
+        {
+            Assembly assembly = typeof(DependencyInjection).Assembly;
+            services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(assembly); });
+            return services;
+        }
+    }
+}
