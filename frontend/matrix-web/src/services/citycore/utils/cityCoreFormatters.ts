5edd9b4f655da6acb102c5df22dc11c018f9ec74
Londongrad <teslagradus@gmail.com>
2026-01-19 15:08:13 +0900

    fix(frontend-citycore): relax City ID validation to canonical GUID format

--- frontend/matrix-web/src/services/citycore/utils/cityCoreFormatters.ts
+++ frontend/matrix-web/src/services/citycore/utils/cityCoreFormatters.ts
@@ -15,9 +15,7 @@
 }
 
 export function isGuid(value: string): boolean {
-    return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
-        value,
-    );
+    return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(value);
 }
 
 export function toIsoUtcFromDatetimeLocal(value: string): string | null {
