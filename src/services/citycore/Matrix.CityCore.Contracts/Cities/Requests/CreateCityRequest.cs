3086caa8fd143a083a8e3e5afa0a41551a39dd94
Londongrad <teslagradus@gmail.com>
2026-01-22 13:20:34 +0900

    feat(citycore-contracts): add city requests
    - Add CreateCityRequest
    - Add RenameCityRequest

---
+++ src/services/citycore/Matrix.CityCore.Contracts/Cities/Requests/CreateCityRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Matrix.CityCore.Contracts.Cities.Requests
+{
+    public sealed record CreateCityRequest(
+        string Name,
+        DateTimeOffset StartSimTimeUtc,
+        decimal SpeedMultiplier = 1.0m);
+}
