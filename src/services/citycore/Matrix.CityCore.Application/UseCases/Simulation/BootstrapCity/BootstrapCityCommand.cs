f9ca177ae23dca8cacc3dbf40bd50390249118d5
Londongrad <teslagradus@gmail.com>
2026-01-22 12:26:12 +0900

    refactor(citycore-application): remove bootstrap city use case

--- src/services/citycore/Matrix.CityCore.Application/UseCases/Simulation/BootstrapCity/BootstrapCityCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-
-namespace Matrix.CityCore.Application.UseCases.Simulation.BootstrapCity
-{
-    public sealed record BootstrapCityCommand(DateTimeOffset StartSimTimeUtc) : IRequest<Guid>;
-}
