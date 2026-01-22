fd5ffcc15b7d898dd58344a1b32af7dc0d2f4bb1
Londongrad <teslagradus@gmail.com>
2026-01-22 13:18:48 +0900

    feat(citycore-application): add CityDto for city read model

---
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/Cities/Common/CityDto.cs
@@ -0,0 +1,24 @@
+﻿using Matrix.CityCore.Domain.Cities;
+
+namespace Matrix.CityCore.Application.UseCases.Cities.Common
+{
+    public sealed record CityDto(
+        Guid CityId,
+        string Name,
+        string Status,
+        DateTimeOffset CreatedAtUtc,
+        DateTimeOffset? ArchivedAtUtc,
+        bool IsArchived)
+    {
+        public static CityDto FromDomain(City city)
+        {
+            return new CityDto(
+                CityId: city.Id.Value,
+                Name: city.Name.Value,
+                Status: city.Status.ToString(),
+                CreatedAtUtc: city.CreatedAtUtc,
+                ArchivedAtUtc: city.ArchivedAtUtc,
+                IsArchived: city.IsArchived);
+        }
+    }
+}
