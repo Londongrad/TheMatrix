8eb5756d501fbca853b2a580ad9cc4ec6e390365
Londongrad <teslagradus@gmail.com>
2026-01-23 19:27:06 +0900

    refactor(gateway): remove redundant contracts, models and mapper

--- src/gateways/Matrix.ApiGateway/DownstreamClients/CityCore/Models/BootstrapCityResponseDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Matrix.ApiGateway.DownstreamClients.CityCore.Models
-{
-    public sealed class BootstrapCityResponseDto
-    {
-        public Guid CityId { get; init; }
-    }
-}
--- src/gateways/Matrix.ApiGateway/DownstreamClients/CityCore/Models/CitySimulationTimeDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Matrix.ApiGateway.DownstreamClients.CityCore.Models
-{
-    public sealed class CitySimulationTimeDto
-    {
-        public DateTimeOffset CurrentTimeUtc { get; init; }
-        public int SimMinutesPerTick { get; init; }
-        public bool IsPaused { get; init; }
-    }
-}
