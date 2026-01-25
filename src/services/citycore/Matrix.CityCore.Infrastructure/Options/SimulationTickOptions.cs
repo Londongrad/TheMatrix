21658576917c0f546b7356c65fd2c5d227981db1
Londongrad <teslagradus@gmail.com>
2026-01-25 11:48:35 +0900

    refactor(citycore): remove DefaultCityId from config and options

--- src/services/citycore/Matrix.CityCore.Infrastructure/Options/SimulationTickOptions.cs
+++ src/services/citycore/Matrix.CityCore.Infrastructure/Options/SimulationTickOptions.cs
@@ -4,7 +4,6 @@
     {
         public const string SectionName = "CityCore:Tick";
 
-        public Guid DefaultCityId { get; set; } = Guid.Parse("11111111-1111-1111-1111-111111111111");
         public int PeriodMilliseconds { get; set; } = 1000;
     }
 }
