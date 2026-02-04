9f7a15b220db40a41f4a3a40a3a1af0414e92faa
Londongrad <teslagradus@gmail.com>
2026-02-04 16:11:07 +0900

    feat(citycore-application): emit city deleted event on city removal

--- src/services/citycore/Matrix.CityCore.Application/UseCases/Cities/DeleteCity/DeleteCityCommandHandler.cs
+++ src/services/citycore/Matrix.CityCore.Application/UseCases/Cities/DeleteCity/DeleteCityCommandHandler.cs
@@ -1,6 +1,8 @@
 using Matrix.BuildingBlocks.Application.Abstractions;
+using Matrix.CityCore.Application.Abstractions.Outbox;
 using Matrix.CityCore.Application.Abstractions.Persistence;
 using Matrix.CityCore.Domain.Cities;
+using Matrix.CityCore.Domain.Events.Cities;
 using MediatR;
 
 namespace Matrix.CityCore.Application.UseCases.Cities.DeleteCity
@@ -8,6 +10,7 @@ namespace Matrix.CityCore.Application.UseCases.Cities.DeleteCity
     public sealed class DeleteCityCommandHandler(
         ICityRepository cityRepository,
         ISimulationClockRepository clockRepository,
+        ICityCoreOutboxWriter outboxWriter,
         IUnitOfWork unitOfWork) : IRequestHandler<DeleteCityCommand, DeleteCityResult>
     {
         public async Task<DeleteCityResult> Handle(
@@ -24,9 +27,19 @@ namespace Matrix.CityCore.Application.UseCases.Cities.DeleteCity
             if (!city.IsArchived)
                 return DeleteCityResult.NotAllowed;
 
+            DateTimeOffset deletedAtUtc = DateTimeOffset.UtcNow;
+
             await unitOfWork.ExecuteInTransactionAsync(
                 action: async ct =>
                 {
+                    await outboxWriter.AddCityEventsAsync(
+                        domainEvents:
+                        [
+                            new CityDeletedDomainEvent(
+                                CityId: city.Id,
+                                DeletedAtUtc: deletedAtUtc)
+                        ],
+                        cancellationToken: ct);
                     await clockRepository.DeleteByCityIdAsync(
                         cityId: city.Id,
                         cancellationToken: ct);
