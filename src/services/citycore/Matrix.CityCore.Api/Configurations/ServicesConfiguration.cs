01f7653a959788d8bec55b1e865f2724cea696c8
Londongrad <teslagradus@gmail.com>
2026-02-17 11:51:31 +0900

    fix(citycore-api): register current user context for mediator permission pipeline

--- src/services/citycore/Matrix.CityCore.Api/Configurations/ServicesConfiguration.cs
+++ src/services/citycore/Matrix.CityCore.Api/Configurations/ServicesConfiguration.cs
@@ -1,4 +1,5 @@
 using Matrix.BuildingBlocks.Api.Authorization;
+using Matrix.BuildingBlocks.Application.Abstractions;
 using Matrix.BuildingBlocks.Application.Authorization.Jwt;
 using Matrix.CityCore.Application;
 using Matrix.CityCore.Infrastructure;
@@ -19,6 +20,8 @@ namespace Matrix.CityCore.Api.Configurations
                 sectionName: InternalJwtOptions.SectionName);
 
             services.AddAuthorization();
+            services.AddHttpContextAccessor();
+            services.AddScoped<ICurrentUserContext, HttpCurrentUserContext>();
 
             services.AddApplication();
             services.AddInfrastructure(configuration);
