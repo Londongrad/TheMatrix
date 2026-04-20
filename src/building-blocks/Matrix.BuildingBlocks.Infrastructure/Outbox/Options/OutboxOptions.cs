315161edd4834a6bdce64be5e298a4f4ba192f59
Londongrad <teslagradus@gmail.com>
2026-04-20 18:18:00 +0900

    chore(outbox): retain processed messages for short diagnostics window

--- src/building-blocks/Matrix.BuildingBlocks.Infrastructure/Outbox/Options/OutboxOptions.cs
+++ src/building-blocks/Matrix.BuildingBlocks.Infrastructure/Outbox/Options/OutboxOptions.cs
@@ -3,12 +3,13 @@ namespace Matrix.BuildingBlocks.Infrastructure.Outbox.Options
     public sealed class OutboxOptions
     {
         public const string SectionName = "Outbox";
+        public const int DefaultProcessedRetentionSeconds = 6 * 60 * 60;
 
         public int BatchSize { get; init; } = 50;
         public int PollIntervalSeconds { get; init; } = 2;
         public int LeaseTtlSeconds { get; init; } = 30;
         public int FailureBackoffMaxSeconds { get; init; } = 300;
-        public int ProcessedRetentionSeconds { get; init; } = 0;
+        public int ProcessedRetentionSeconds { get; init; } = DefaultProcessedRetentionSeconds;
         public int CleanupBatchSize { get; init; } = 500;
         public bool DispatcherEnabled { get; init; } = true;
     }
