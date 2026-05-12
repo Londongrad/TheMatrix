c3c96510070a727ae7729635f5fbf7f3cc25c480
Londongrad <teslagradus@gmail.com>
2026-05-12 20:46:00 +0900

    test(simulationsystems): cover outbox publisher guards

---
+++ tests/services/simulationsystems/Matrix.SimulationSystems.Infrastructure.Tests/Outbox/MassTransitOutboxMessagePublisherTests.cs
@@ -0,0 +1,52 @@
+using System.Text.Json;
+using Matrix.SimulationSystems.Infrastructure.Outbox.RabbitMq;
+using Matrix.SimulationSystems.Infrastructure.Outbox;
+using Xunit;
+
+namespace Matrix.SimulationSystems.Infrastructure.Tests.Outbox;
+
+public sealed class MassTransitOutboxMessagePublisherTests
+{
+    [Fact]
+    public async Task PublishAsync_WhenMessageTypeIsUnsupported_ThrowsNotSupportedException()
+    {
+        var publisher = new MassTransitOutboxMessagePublisher(null!);
+
+        NotSupportedException exception = await Assert.ThrowsAsync<NotSupportedException>(
+            () => publisher.PublishAsync(
+                messageId: Guid.Parse("f0529b17-74bb-4e28-b0e1-0b436eeb2b60"),
+                type: "simulationsystems.unknown.v1",
+                payloadJson: "{}",
+                cancellationToken: CancellationToken.None));
+
+        Assert.Contains("not supported", exception.Message);
+    }
+
+    [Fact]
+    public async Task PublishAsync_WhenPayloadDeserializesToNull_ThrowsInvalidOperationException()
+    {
+        var publisher = new MassTransitOutboxMessagePublisher(null!);
+
+        InvalidOperationException exception = await Assert.ThrowsAsync<InvalidOperationException>(
+            () => publisher.PublishAsync(
+                messageId: Guid.Parse("0a4ae14d-2067-4fa4-9b57-ebd69f1ec11d"),
+                type: SimulationSystemsOutboxEventTypes.ClassicCityLivingConditionsSnapshotV1,
+                payloadJson: "null",
+                cancellationToken: CancellationToken.None));
+
+        Assert.Contains("Failed to deserialize outbox payload", exception.Message);
+    }
+
+    [Fact]
+    public async Task PublishAsync_WhenPayloadJsonIsMalformed_ThrowsJsonException()
+    {
+        var publisher = new MassTransitOutboxMessagePublisher(null!);
+
+        await Assert.ThrowsAsync<JsonException>(
+            () => publisher.PublishAsync(
+                messageId: Guid.Parse("56131248-e854-4ecc-a7f3-31ddb4d3d2b5"),
+                type: SimulationSystemsOutboxEventTypes.ClassicCityOperationalExpenseIncurredV1,
+                payloadJson: "{",
+                cancellationToken: CancellationToken.None));
+    }
+}
