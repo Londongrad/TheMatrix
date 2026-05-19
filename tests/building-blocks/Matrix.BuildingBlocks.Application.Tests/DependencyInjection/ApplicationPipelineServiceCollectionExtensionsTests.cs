92b3a0972313f44f9ada587335acd67ec1d250e2
Londongrad <teslagradus@gmail.com>
2026-05-19 12:24:00 +0900

    test(shared): cover building blocks application pipeline registration

---
+++ tests/building-blocks/Matrix.BuildingBlocks.Application.Tests/DependencyInjection/ApplicationPipelineServiceCollectionExtensionsTests.cs
@@ -0,0 +1,32 @@
+using Matrix.BuildingBlocks.Application.Behaviors;
+using Matrix.BuildingBlocks.Application.DependencyInjection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Matrix.BuildingBlocks.Application.Tests.DependencyInjection;
+
+public sealed class ApplicationPipelineServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddDefaultApplicationPipeline_WhenCalled_RegistersDefaultBehaviors()
+    {
+        ServiceCollection services = new();
+
+        services.AddDefaultApplicationPipeline();
+
+        Type[] implementations = services
+            .Where(descriptor => descriptor.ServiceType == typeof(IPipelineBehavior<,>))
+            .Select(descriptor => descriptor.ImplementationType)
+            .OfType<Type>()
+            .ToArray();
+
+        Assert.Equal(
+            [
+                typeof(LoggingBehavior<,>),
+                typeof(PermissionBehavior<,>),
+                typeof(ValidationBehavior<,>)
+            ],
+            implementations);
+    }
+}
