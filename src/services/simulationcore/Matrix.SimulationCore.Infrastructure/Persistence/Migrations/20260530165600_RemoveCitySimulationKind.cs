73253d563e5b5cde1dfd67a25952abfcb83ac3d3
Londongrad <teslagradus@gmail.com>
2026-05-30 16:56:00 +0300

    db(simulation-core): drop city simulation kind column

---
+++ src/services/simulationcore/Matrix.SimulationCore.Infrastructure/Persistence/Migrations/20260530165600_RemoveCitySimulationKind.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matrix.SimulationCore.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCitySimulationKind : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SimulationKind",
+                table: "Cities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SimulationKind",
+                table: "Cities",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+        }
+    }
+}
