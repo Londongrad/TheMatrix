c5de307893b33b6887a04e40a511168e6aa28f18
Londongrad <teslagradus@gmail.com>
2026-06-05 19:17:00 +0300

    refactor(gateway): remove city residents from shared client

--- src/gateways/Matrix.ApiGateway/DownstreamClients/Population/People/PopulationApiClient.cs
+++ src/gateways/Matrix.ApiGateway/DownstreamClients/Population/People/PopulationApiClient.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using Matrix.ApiGateway.DownstreamClients.Common;
 using Matrix.ApiGateway.DownstreamClients.Common.Extensions;
 using Matrix.BuildingBlocks.Application.Models;
@@ -17,57 +16,6 @@ namespace Matrix.ApiGateway.DownstreamClients.Population.People
         #endregion [ Fields ]
 
         #region [ Methods ]
-
-        public async Task<PagedResult<PersonDto>> GetCityResidentsPageAsync(
-            Guid cityId,
-            DateOnly currentDate,
-            int pageNumber,
-            int pageSize,
-            CancellationToken cancellationToken = default)
-        {
-            string currentDateValue = Uri.EscapeDataString(
-                stringToEscape: currentDate.ToString(
-                    format: "yyyy-MM-dd",
-                    provider: CultureInfo.InvariantCulture));
-            string url =
-                $"{PopulationBaseEndpoint}/cities/{cityId}/residents?currentDate={currentDateValue}&pageNumber={pageNumber}&pageSize={pageSize}";
-
-            using HttpResponseMessage response = await _client.GetAsync(
-                requestUri: url,
-                cancellationToken: cancellationToken);
-
-            await response.EnsureSuccessOrThrowDownstreamAsync(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken);
-
-            PagedResult<PersonDto>? result = await response.Content
-               .ReadFromJsonAsync<PagedResult<PersonDto>>(cancellationToken: cancellationToken);
-
-            return result ?? throw new InvalidOperationException("Empty response from Population API.");
-        }
-
-        public async Task<CityResidentDetailsDto> GetCityResidentDetailsAsync(
-            Guid cityId,
-            Guid personId,
-            DateOnly currentDate,
-            CancellationToken cancellationToken = default)
-        {
-            string currentDateValue = Uri.EscapeDataString(
-                stringToEscape: currentDate.ToString(
-                    format: "yyyy-MM-dd",
-                    provider: CultureInfo.InvariantCulture));
-            string url =
-                $"{PopulationBaseEndpoint}/cities/{cityId}/residents/{personId}?currentDate={currentDateValue}";
-
-            using HttpResponseMessage response = await _client.GetAsync(
-                requestUri: url,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityResidentDetailsDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
 
         public async Task<CityEmploymentCatalogDto> GetCityEmploymentCatalogAsync(
             Guid cityId,
