9d40f6e990ef92b5f19e3a4cf1181bcb447d5990
Londongrad <teslagradus@gmail.com>
2026-06-05 18:38:00 +0300

    refactor(gateway): remove city population summaries from shared client

--- src/gateways/Matrix.ApiGateway/DownstreamClients/Population/People/PopulationApiClient.cs
+++ src/gateways/Matrix.ApiGateway/DownstreamClients/Population/People/PopulationApiClient.cs
@@ -18,71 +18,6 @@ namespace Matrix.ApiGateway.DownstreamClients.Population.People
 
         #region [ Methods ]
 
-        public async Task<CityPopulationBootstrapSummaryDto> InitializeCityPopulationAsync(
-            InitializeCityPopulationRequest request,
-            CancellationToken cancellationToken = default)
-        {
-            const string url = InitializeEndpoint;
-
-            using HttpResponseMessage response = await _client.PostAsJsonAsync(
-                requestUri: url,
-                value: request,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityPopulationBootstrapSummaryDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
-        public async Task<CityPopulationSummaryDto> GetCityPopulationSummaryAsync(
-            Guid cityId,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/summary";
-
-            using HttpResponseMessage response = await _client.GetAsync(
-                requestUri: url,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityPopulationSummaryDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
-        public async Task<CityPopulationDashboardDto> GetCityPopulationDashboardAsync(
-            Guid cityId,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/dashboard";
-
-            using HttpResponseMessage response = await _client.GetAsync(
-                requestUri: url,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityPopulationDashboardDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
-        public async Task<CityPopulationDistrictPressureDto> GetCityDistrictPressureAsync(
-            Guid cityId,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/district-pressure";
-
-            using HttpResponseMessage response = await _client.GetAsync(
-                requestUri: url,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityPopulationDistrictPressureDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
         public async Task<PagedResult<PersonDto>> GetCityResidentsPageAsync(
             Guid cityId,
             DateOnly currentDate,
@@ -341,7 +276,6 @@ namespace Matrix.ApiGateway.DownstreamClients.Population.People
 
         private const string PopulationBaseEndpoint = "/api/population";
 
-        private const string InitializeEndpoint = PopulationBaseEndpoint + "/init";
         private const string GetPagedEndpoint = PopulationBaseEndpoint + "/citizens";
 
         #endregion [ Constants ]
