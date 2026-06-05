e5ea338d9bd25e4a52a2fa3b7dcec69b369e81e7
Londongrad <teslagradus@gmail.com>
2026-06-05 19:56:00 +0300

    refactor(gateway): remove city employment from shared client

--- src/gateways/Matrix.ApiGateway/DownstreamClients/Population/People/PopulationApiClient.cs
+++ src/gateways/Matrix.ApiGateway/DownstreamClients/Population/People/PopulationApiClient.cs
@@ -17,22 +17,6 @@ namespace Matrix.ApiGateway.DownstreamClients.Population.People
 
         #region [ Methods ]
 
-        public async Task<CityEmploymentCatalogDto> GetCityEmploymentCatalogAsync(
-            Guid cityId,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/employment/catalog";
-
-            using HttpResponseMessage response = await _client.GetAsync(
-                requestUri: url,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityEmploymentCatalogDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
         public async Task<CityEducationCatalogDto> GetCityEducationCatalogAsync(
             Guid cityId,
             CancellationToken cancellationToken = default)
@@ -44,60 +28,6 @@ namespace Matrix.ApiGateway.DownstreamClients.Population.People
                 cancellationToken: cancellationToken);
 
             return await response.ReadJsonOrThrowDownstreamAsync<CityEducationCatalogDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
-        public async Task<CityEmploymentOperationResultDto> HireCityResidentAsync(
-            Guid cityId,
-            CityEmploymentOperationRequest request,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/employment/hire";
-
-            using HttpResponseMessage response = await _client.PostAsJsonAsync(
-                requestUri: url,
-                value: request,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityEmploymentOperationResultDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
-        public async Task<CityEmploymentOperationResultDto> FireCityResidentAsync(
-            Guid cityId,
-            CityEmploymentOperationRequest request,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/employment/fire";
-
-            using HttpResponseMessage response = await _client.PostAsJsonAsync(
-                requestUri: url,
-                value: request,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityEmploymentOperationResultDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
-        public async Task<CityEmploymentOperationResultDto> RetireCityResidentAsync(
-            Guid cityId,
-            CityEmploymentOperationRequest request,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/employment/retire";
-
-            using HttpResponseMessage response = await _client.PostAsJsonAsync(
-                requestUri: url,
-                value: request,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityEmploymentOperationResultDto>(
                 serviceName: ServiceName,
                 cancellationToken: cancellationToken,
                 requestUrl: url);
