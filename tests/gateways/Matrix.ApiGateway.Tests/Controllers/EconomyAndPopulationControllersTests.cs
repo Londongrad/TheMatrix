a0c537f503c32e2b9af03c460eaf03cea8d0b841
Londongrad <teslagradus@gmail.com>
2026-06-06 09:32:00 +0300

    test(classic-city): cover population gateway route

--- tests/gateways/Matrix.ApiGateway.Tests/Controllers/EconomyAndPopulationControllersTests.cs
+++ tests/gateways/Matrix.ApiGateway.Tests/Controllers/EconomyAndPopulationControllersTests.cs
@@ -37,6 +37,21 @@ namespace Matrix.ApiGateway.Tests.Controllers
         }
 
         [Fact]
+        public void ClassicCityPopulationController_UsesScenarioRoute()
+        {
+            RouteAttribute route = Assert.Single(
+                typeof(ClassicCityPopulationController)
+                   .GetCustomAttributes(
+                        attributeType: typeof(RouteAttribute),
+                        inherit: true)
+                   .Cast<RouteAttribute>());
+
+            Assert.Equal(
+                expected: "api/scenarios/classic-city/population",
+                actual: route.Template);
+        }
+
+        [Fact]
         public async Task EconomyControllerGetSummary_WhenDownstreamReturnsNull_MapsBadGateway()
         {
             var controller = new EconomyController(
