6ef9d22d1df53b315d2b015aba0d7cbcb30e1679
Londongrad <teslagradus@gmail.com>
2026-06-06 08:14:00 +0300

    refactor(gateway): remove city civil registry from shared client

--- src/gateways/Matrix.ApiGateway/DownstreamClients/Population/People/PopulationApiClient.cs
+++ src/gateways/Matrix.ApiGateway/DownstreamClients/Population/People/PopulationApiClient.cs
@@ -2,7 +2,6 @@ using Matrix.ApiGateway.DownstreamClients.Common;
 using Matrix.ApiGateway.DownstreamClients.Common.Extensions;
 using Matrix.BuildingBlocks.Application.Models;
 using Matrix.Population.Contracts.Models;
-using Matrix.Population.Contracts.Scenarios.ClassicCity.Models;
 
 namespace Matrix.ApiGateway.DownstreamClients.Population.People
 {
@@ -16,42 +15,6 @@ namespace Matrix.ApiGateway.DownstreamClients.Population.People
         #endregion [ Fields ]
 
         #region [ Methods ]
-
-        public async Task<CityCivilRegistryOperationResultDto> RegisterCityMarriageAsync(
-            Guid cityId,
-            CityCivilRegistryOperationRequest request,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/civil-registry/marriages";
-
-            using HttpResponseMessage response = await _client.PostAsJsonAsync(
-                requestUri: url,
-                value: request,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityCivilRegistryOperationResultDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
-
-        public async Task<CityCivilRegistryOperationResultDto> RegisterCityDivorceAsync(
-            Guid cityId,
-            CityCivilRegistryOperationRequest request,
-            CancellationToken cancellationToken = default)
-        {
-            string url = $"{PopulationBaseEndpoint}/cities/{cityId}/civil-registry/divorces";
-
-            using HttpResponseMessage response = await _client.PostAsJsonAsync(
-                requestUri: url,
-                value: request,
-                cancellationToken: cancellationToken);
-
-            return await response.ReadJsonOrThrowDownstreamAsync<CityCivilRegistryOperationResultDto>(
-                serviceName: ServiceName,
-                cancellationToken: cancellationToken,
-                requestUrl: url);
-        }
 
         public async Task<PagedResult<PersonDto>> GetCitizensPageAsync(
             int pageNumber,
