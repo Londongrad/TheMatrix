74331c7b01499b47c11c80738cd83260d26b8f89
Londongrad <teslagradus@gmail.com>
2026-06-07 19:56:00 +0300

    refactor(classic-city): scope internal city api

--- src/services/simulationcore/Matrix.SimulationCore.Contracts/Scenarios/ClassicCity/ClassicCityApiRoutes.cs
+++ src/services/simulationcore/Matrix.SimulationCore.Contracts/Scenarios/ClassicCity/ClassicCityApiRoutes.cs
@@ -2,7 +2,7 @@ namespace Matrix.SimulationCore.Contracts.Scenarios.ClassicCity;
 
 public static class ClassicCityApiRoutes
 {
-    public const string CitiesRoute = "api/cities";
+    public const string CitiesRoute = "api/scenarios/classic-city/cities";
     public const string CitiesPath = "/" + CitiesRoute;
     public const string TripsRoute = CitiesRoute + "/{cityId:guid}/trips";
     public const string RoutingRoute = CitiesRoute + "/{cityId:guid}/routes";
