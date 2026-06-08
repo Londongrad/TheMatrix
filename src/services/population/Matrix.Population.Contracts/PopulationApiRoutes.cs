e07090dcbf65c1a69e741012a9d696f8df143bc9
Londongrad <teslagradus@gmail.com>
2026-06-08 16:18:00 +0300

    refactor(population): expose scenario-neutral people api

--- src/services/population/Matrix.Population.Contracts/PopulationApiRoutes.cs
+++ src/services/population/Matrix.Population.Contracts/PopulationApiRoutes.cs
@@ -3,8 +3,8 @@ namespace Matrix.Population.Contracts;
 public static class PopulationApiRoutes
 {
     public const string PopulationRoute = "api/population";
-    public const string PeopleRoute = PopulationRoute + "/citizens";
+    public const string PeopleRoute = PopulationRoute + "/people";
     public const string PeoplePath = "/" + PeopleRoute;
-    public const string PersonRoute = "api/person/{personId:guid}";
-    public const string PersonPath = "/api/person";
+    public const string PersonRoute = PeopleRoute + "/{personId:guid}";
+    public const string PersonPath = PeoplePath;
 }
