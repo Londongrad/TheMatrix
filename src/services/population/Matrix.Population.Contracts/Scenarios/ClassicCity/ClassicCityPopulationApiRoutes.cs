9eac61358e9519986c40d338531781bf7f7edc67
Londongrad <teslagradus@gmail.com>
2026-06-08 18:22:00 +0300

    refactor(classic-city): define population api routes

---
+++ src/services/population/Matrix.Population.Contracts/Scenarios/ClassicCity/ClassicCityPopulationApiRoutes.cs
@@ -0,0 +1,9 @@
+namespace Matrix.Population.Contracts.Scenarios.ClassicCity;
+
+public static class ClassicCityPopulationApiRoutes
+{
+    public const string PopulationRoute = "api/population";
+    public const string PopulationPath = "/" + PopulationRoute;
+    public const string InitializePath = PopulationPath + "/init";
+    public const string CitiesPath = PopulationPath + "/cities";
+}
