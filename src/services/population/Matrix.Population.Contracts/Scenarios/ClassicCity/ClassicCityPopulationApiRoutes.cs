19c6b5b209fb0422d493b2864b4e1fe6f40f873a
Londongrad <teslagradus@gmail.com>
2026-06-09 08:12:00 +0300

    refactor(classic-city): model population route hierarchy

--- src/services/population/Matrix.Population.Contracts/Scenarios/ClassicCity/ClassicCityPopulationApiRoutes.cs
+++ src/services/population/Matrix.Population.Contracts/Scenarios/ClassicCity/ClassicCityPopulationApiRoutes.cs
@@ -5,5 +5,11 @@ public static class ClassicCityPopulationApiRoutes
     public const string PopulationRoute = "api/scenarios/classic-city/population";
     public const string PopulationPath = "/" + PopulationRoute;
     public const string InitializePath = PopulationPath + "/init";
-    public const string CitiesPath = PopulationPath + "/cities";
+    public const string CitiesRoute = PopulationRoute + "/cities";
+    public const string CitiesPath = "/" + CitiesRoute;
+    public const string CityRoute = CitiesRoute + "/{cityId:guid}";
+    public const string ResidentsRoute = CityRoute + "/residents";
+    public const string EmploymentRoute = CityRoute + "/employment";
+    public const string EducationRoute = CityRoute + "/education";
+    public const string CivilRegistryRoute = CityRoute + "/civil-registry";
 }
