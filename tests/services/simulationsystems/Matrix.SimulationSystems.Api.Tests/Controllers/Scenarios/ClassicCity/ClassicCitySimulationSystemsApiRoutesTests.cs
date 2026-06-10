e27d7080c7420e095995189ac7c0380dc4e0dc9f
Londongrad <teslagradus@gmail.com>
2026-06-10 16:51:00 +0300

    test(classic-city): guard simulation controller routes

---
+++ tests/services/simulationsystems/Matrix.SimulationSystems.Api.Tests/Controllers/Scenarios/ClassicCity/ClassicCitySimulationSystemsApiRoutesTests.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using Matrix.SimulationSystems.Api.Controllers.Scenarios.ClassicCity;
+using Matrix.SimulationSystems.Contracts.Scenarios.ClassicCity;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Matrix.SimulationSystems.Api.Tests.Controllers.Scenarios.ClassicCity
+{
+    public sealed class ClassicCitySimulationSystemsApiRoutesTests
+    {
+        public static TheoryData<Type> ControllerTypes =>
+            new()
+            {
+                typeof(DrainageController),
+                typeof(EnvironmentalConditionsController),
+                typeof(HeatingController),
+                typeof(PowerDistributionController),
+                typeof(RoadAccessController),
+                typeof(SanitationController),
+                typeof(SnowRemovalController),
+                typeof(UtilityIncidentsController),
+                typeof(WaterDistributionController)
+            };
+
+        [Theory]
+        [MemberData(nameof(ControllerTypes))]
+        public void Controller_UsesScenarioRouteContract(Type controllerType)
+        {
+            RouteAttribute? route = controllerType.GetCustomAttribute<RouteAttribute>();
+
+            Assert.NotNull(route);
+            Assert.Equal(
+                expected: ClassicCitySimulationSystemsApiRoutes.CitiesRoute,
+                actual: route.Template);
+        }
+    }
+}
