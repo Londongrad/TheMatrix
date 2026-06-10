c966c8328aa17b884b0f1a10431bf477638aa075
Londongrad <teslagradus@gmail.com>
2026-06-10 08:11:00 +0300

    refactor(classic-city): define simulation systems api routes

---
+++ src/services/simulationsystems/Matrix.SimulationSystems.Contracts/Scenarios/ClassicCity/ClassicCitySimulationSystemsApiRoutes.cs
@@ -0,0 +1,17 @@
+namespace Matrix.SimulationSystems.Contracts.Scenarios.ClassicCity;
+
+public static class ClassicCitySimulationSystemsApiRoutes
+{
+    public const string CitiesRoute = "api/classic-city/cities";
+    public const string CitiesPath = "/" + CitiesRoute;
+
+    public const string EnvironmentalConditionsSegment = "environmental-conditions";
+    public const string HeatingSegment = "heating";
+    public const string WaterDistributionSegment = "water-distribution";
+    public const string PowerDistributionSegment = "power-distribution";
+    public const string SanitationSegment = "sanitation";
+    public const string UtilityIncidentsSegment = "utility-incidents";
+    public const string RoadAccessSegment = "road-access";
+    public const string DrainageSegment = "drainage";
+    public const string SnowRemovalSegment = "snow-removal";
+}
