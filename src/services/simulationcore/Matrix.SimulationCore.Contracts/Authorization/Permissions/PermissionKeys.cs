ce5afa75a7dad994253ecfb278320aff906ea563
Londongrad <teslagradus@gmail.com>
2026-06-12 21:44:00 +0300

    refactor(permissions): remove common classic city aliases

--- src/services/simulationcore/Matrix.SimulationCore.Contracts/Authorization/Permissions/PermissionKeys.cs
+++ src/services/simulationcore/Matrix.SimulationCore.Contracts/Authorization/Permissions/PermissionKeys.cs
@@ -4,15 +4,6 @@ namespace Matrix.SimulationCore.Contracts.Authorization.Permissions
     {
         public const string SimulationCoreScenariosCatalogRead = "simulationcore.scenarios.catalog.read";
 
-        public const string SimulationCoreClassicCityRead = "simulationcore.classic-city.read";
-        public const string SimulationCoreClassicCityCreate = "simulationcore.classic-city.create";
-        public const string SimulationCoreClassicCityUpdate = "simulationcore.classic-city.update";
-        public const string SimulationCoreClassicCityArchive = "simulationcore.classic-city.archive";
-        public const string SimulationCoreClassicCityDelete = "simulationcore.classic-city.delete";
-
-        public const string SimulationCoreClassicCityPopulationBootstrapRetry =
-            "simulationcore.classic-city.population-bootstrap.retry";
-
         public const string SimulationCoreSimulationRead = "simulationcore.simulations.read";
         public const string SimulationCoreSimulationControl = "simulationcore.simulations.control";
     }
