676f213b551532312de7d43a05a98f1cc977c6ce
Londongrad <teslagradus@gmail.com>
2026-06-13 16:57:00 +0300

    refactor(economy): isolate scenario db sets

--- src/services/economy/Matrix.Economy.Infrastructure/Persistence/EconomyDbContext.cs
+++ src/services/economy/Matrix.Economy.Infrastructure/Persistence/EconomyDbContext.cs
@@ -1,30 +1,12 @@
 using Matrix.BuildingBlocks.Infrastructure.Outbox.Models;
 using Matrix.BuildingBlocks.Infrastructure.Outbox.Persistence;
-using Matrix.Economy.Domain.Aggregates;
-using Matrix.Economy.Domain.Entities;
-using Matrix.Economy.Infrastructure.Scenarios.ClassicCity.Persistence.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Matrix.Economy.Infrastructure.Persistence
 {
-    public class EconomyDbContext(DbContextOptions<EconomyDbContext> options)
+    public partial class EconomyDbContext(DbContextOptions<EconomyDbContext> options)
         : DbContext(options)
     {
-        public DbSet<CityBusiness> CityBusinesses => Set<CityBusiness>();
-        public DbSet<CityBusinessLedgerEntry> CityBusinessLedgerEntries => Set<CityBusinessLedgerEntry>();
-        public DbSet<CityHouseholdAccount> CityHouseholdAccounts => Set<CityHouseholdAccount>();
-
-        public DbSet<CityHouseholdAccountLedgerEntry> CityHouseholdAccountLedgerEntries
-            => Set<CityHouseholdAccountLedgerEntry>();
-
-        public DbSet<CityHouseholdObligation> CityHouseholdObligations => Set<CityHouseholdObligation>();
-        public DbSet<CityBudget> CityBudgets => Set<CityBudget>();
-        public DbSet<CityBudgetAllocation> CityBudgetAllocations => Set<CityBudgetAllocation>();
-        public DbSet<CityBudgetLedgerEntry> CityBudgetLedgerEntries => Set<CityBudgetLedgerEntry>();
-        public DbSet<CityEconomyCostProfileState> CityEconomyCostProfileStates => Set<CityEconomyCostProfileState>();
-        public DbSet<CityEconomyProgressionState> CityEconomyProgressionStates => Set<CityEconomyProgressionState>();
-        public DbSet<CityBudgetSettlement> CityBudgetSettlements => Set<CityBudgetSettlement>();
-        public DbSet<CityEconomyDeletionState> CityEconomyDeletionStates => Set<CityEconomyDeletionState>();
         public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ src/services/economy/Matrix.Economy.Infrastructure/Scenarios/ClassicCity/Persistence/EconomyDbContext.ClassicCity.cs
@@ -0,0 +1,26 @@
+using Matrix.Economy.Domain.Aggregates;
+using Matrix.Economy.Domain.Entities;
+using Matrix.Economy.Infrastructure.Scenarios.ClassicCity.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Matrix.Economy.Infrastructure.Persistence
+{
+    public partial class EconomyDbContext
+    {
+        public DbSet<CityBusiness> CityBusinesses => Set<CityBusiness>();
+        public DbSet<CityBusinessLedgerEntry> CityBusinessLedgerEntries => Set<CityBusinessLedgerEntry>();
+        public DbSet<CityHouseholdAccount> CityHouseholdAccounts => Set<CityHouseholdAccount>();
+
+        public DbSet<CityHouseholdAccountLedgerEntry> CityHouseholdAccountLedgerEntries
+            => Set<CityHouseholdAccountLedgerEntry>();
+
+        public DbSet<CityHouseholdObligation> CityHouseholdObligations => Set<CityHouseholdObligation>();
+        public DbSet<CityBudget> CityBudgets => Set<CityBudget>();
+        public DbSet<CityBudgetAllocation> CityBudgetAllocations => Set<CityBudgetAllocation>();
+        public DbSet<CityBudgetLedgerEntry> CityBudgetLedgerEntries => Set<CityBudgetLedgerEntry>();
+        public DbSet<CityEconomyCostProfileState> CityEconomyCostProfileStates => Set<CityEconomyCostProfileState>();
+        public DbSet<CityEconomyProgressionState> CityEconomyProgressionStates => Set<CityEconomyProgressionState>();
+        public DbSet<CityBudgetSettlement> CityBudgetSettlements => Set<CityBudgetSettlement>();
+        public DbSet<CityEconomyDeletionState> CityEconomyDeletionStates => Set<CityEconomyDeletionState>();
+    }
+}
