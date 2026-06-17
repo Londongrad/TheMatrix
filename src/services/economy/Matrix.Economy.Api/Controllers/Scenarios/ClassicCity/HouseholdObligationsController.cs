1de95b279f54ed8dc93d5457e05f09142b1451c3
Londongrad <teslagradus@gmail.com>
2026-06-17 19:03:00 +0300

    refactor(economy): isolate household obligation controller

--- src/services/economy/Matrix.Economy.Api/Controllers/Scenarios/ClassicCity/HouseholdObligationsController.cs
+++ src/services/economy/Matrix.Economy.Api/Controllers/Scenarios/ClassicCity/HouseholdObligationsController.cs
@@ -11,7 +11,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Matrix.Economy.Api.Controllers
+namespace Matrix.Economy.Api.Controllers.Scenarios.ClassicCity
 {
     [ApiController]
     [Authorize]
