709459b6cfd541bbe1dc41e8160826af51f35116
Londongrad <teslagradus@gmail.com>
2026-06-18 11:50:00 +0300

    refactor(events): isolate workplace business sync events

--- src/building-blocks/Matrix.BuildingBlocks.Application/IntegrationEvents/Scenarios/ClassicCity/Economy/ClassicCityWorkplaceBusinessSyncItemV1.cs
+++ src/building-blocks/Matrix.BuildingBlocks.Application/IntegrationEvents/Scenarios/ClassicCity/Economy/ClassicCityWorkplaceBusinessSyncItemV1.cs
@@ -1,4 +1,4 @@
-namespace Matrix.BuildingBlocks.Application.IntegrationEvents.Economy
+namespace Matrix.BuildingBlocks.Application.IntegrationEvents.Scenarios.ClassicCity.Economy
 {
     public sealed record ClassicCityWorkplaceBusinessSyncItemV1(
         Guid WorkplaceId,
