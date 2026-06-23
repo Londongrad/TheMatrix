5291bfac39e2458c78affc7fa233b28d95c5ab4d
Londongrad <teslagradus@gmail.com>
2026-06-23 13:19:00 +0300

    feat(education): register application handlers

---
+++ src/services/education/Matrix.Education.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Matrix.Education.Application
+{
+    public static class DependencyInjection
+    {
+        public static void AddEducationApplication(this IServiceCollection services)
+        {
+            Assembly assembly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+        }
+    }
+}
