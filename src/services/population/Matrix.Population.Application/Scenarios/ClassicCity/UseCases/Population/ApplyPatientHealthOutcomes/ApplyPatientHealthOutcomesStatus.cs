4ccdc6344ea6db1224840f4ade58dd57b170ffea
Londongrad <teslagradus@gmail.com>
2026-06-29 15:15:00 +0300

    feat(population): define healthcare outcome application contract

---
+++ src/services/population/Matrix.Population.Application/Scenarios/ClassicCity/UseCases/Population/ApplyPatientHealthOutcomes/ApplyPatientHealthOutcomesStatus.cs
@@ -0,0 +1,10 @@
+namespace Matrix.Population.Application.Scenarios.ClassicCity.UseCases.Population.ApplyPatientHealthOutcomes
+{
+    public enum ApplyPatientHealthOutcomesStatus
+    {
+        Applied = 1,
+        Duplicate = 2,
+        CityDeleted = 3,
+        CityArchived = 4
+    }
+}
