aa2f13c9d02b9ba94ac082a5ec2b96a24be52f2d
Londongrad <teslagradus@gmail.com>
2026-07-07 10:29:00 +0300

    feat(population): migrate functional capacity

---
+++ src/services/population/Matrix.Population.Infrastructure/Persistence/Migrations/20260707102900_AddResidentFunctionalCapacity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matrix.Population.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResidentFunctionalCapacity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FunctionalCapacity",
+                table: "Persons",
+                type: "integer",
+                nullable: false,
+                defaultValue: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FunctionalCapacity",
+                table: "Persons");
+        }
+    }
+}
