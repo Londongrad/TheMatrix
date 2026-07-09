4e8e48e5231901a83c4b306ff0036b46c7bbacf2
Londongrad <teslagradus@gmail.com>
2026-07-09 20:41:00 +0300

    refactor(population): migrate vital revision rename

---
+++ src/services/population/Matrix.Population.Infrastructure/Persistence/Migrations/20260709204100_RenameResidentVitalStateRevision.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matrix.Population.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameResidentVitalStateRevision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastHealthcareRevision",
+                table: "Persons",
+                newName: "LastVitalStateRevision");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastVitalStateRevision",
+                table: "Persons",
+                newName: "LastHealthcareRevision");
+        }
+    }
+}
