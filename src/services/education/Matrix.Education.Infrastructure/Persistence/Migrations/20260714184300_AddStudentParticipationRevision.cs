c99b32e851aeb2fcee482a194e171b2a7af29e90
Londongrad <teslagradus@gmail.com>
2026-07-14 18:43:00 +0300

    db(education): persist participation revisions

---
+++ src/services/education/Matrix.Education.Infrastructure/Persistence/Migrations/20260714184300_AddStudentParticipationRevision.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matrix.Education.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentParticipationRevision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "participation_revision",
+                table: "education_student_profiles",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "participation_revision",
+                table: "education_student_profiles");
+        }
+    }
+}
